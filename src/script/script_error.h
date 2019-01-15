34ce09a16d5758ae2ef9849a8d2b59bfd4236049
Andrew Stone <g.andrew.stone@gmail.com>
2019-01-15 13:24:55 -0500

    Refactor Script interpreter as a "virtual machine" encapsulated by a class.
    Refactor script interpreter into a virtual machine, add access via cashlib and python, and more precise script error reporting.

--- src/script/script_error.h
+++ src/script/script_error.h
@@ -64,6 +64,10 @@ typedef enum ScriptError_t
     /* softfork safeness */
     SCRIPT_ERR_DISCOURAGE_UPGRADABLE_NOPS,
 
+    /* misc */
+    SCRIPT_ERR_NUMBER_OVERFLOW,
+    SCRIPT_ERR_NUMBER_BAD_ENCODING,
+
     /* Constant scriptCode */
     SCRIPT_ERR_OP_CODESEPARATOR,
     SCRIPT_ERR_SIG_FINDANDDELETE,
