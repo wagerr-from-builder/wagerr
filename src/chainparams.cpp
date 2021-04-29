8c1f2e6971a73ab025f922def0d43e00a7062ed7
celbalrai <80897309+celbalrai@users.noreply.github.com>
2021-04-29 15:11:23 +0200

    Update chain parameters (V16 heights)

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -584,7 +584,7 @@ public:
         consensus.nBlockStakeModifierV1A = 51197;
         consensus.nBlockStakeModifierV2 = std::numeric_limits<int>::max();
         // ATP parameters
-        consensus.ATPStartHeight = std::numeric_limits<int64_t>::max();
+        consensus.ATPStartHeight = consensus.V16DeploymentHeight;
         consensus.WagerrAddrPrefix = "wagerrtest";
         consensus.strTokenManagementKey = "TkDutp66Ygp5PpPnrETvfyrtnxq5UevLpo";
         consensus.nOpGroupNewRequiredConfirmations = 1;
@@ -761,7 +761,7 @@ public:
         consensus.nBlockStakeModifierV1A = 1000;
         consensus.nBlockStakeModifierV2 = consensus.V16DeploymentHeight;
         // ATP parameters
-        consensus.ATPStartHeight = std::numeric_limits<int64_t>::max();
+        consensus.ATPStartHeight = consensus.V16DeploymentHeight;
         consensus.WagerrAddrPrefix = "wagerrtest";
         consensus.strTokenManagementKey = "TkDutp66Ygp5PpPnrETvfyrtnxq5UevLpo";
         consensus.nOpGroupNewRequiredConfirmations = 1;
@@ -907,7 +907,7 @@ public:
         consensus.V16DeploymentHeight = 300;
         consensus.BIP34Height = 100000000; // BIP34 has not activated on regtest (far in the future so block v1 are not rejected in tests)
         consensus.BIP34Hash = uint256();
-        consensus.BIP65Height = 1351; // BIP65 activated on regtest (Used in rpc activation tests)
+        consensus.BIP65Height = consensus.V16DeploymentHeight; // BIP65 activated on regtest (Used in rpc activation tests)
         consensus.BIP66Height = 1251; // BIP66 activated on regtest (Used in rpc activation tests)
         consensus.DIP0001Height = 2000;
         consensus.DIP0003Height = 432;
@@ -932,10 +932,10 @@ public:
         consensus.nPosTargetTimespan_V2 = 2 * consensus.nTimeSlotLength * 60; // 30 minutes
         consensus.nStakeMinDepth = 100;
         consensus.nStakeMinAge = 0;
-        consensus.nBlockStakeModifierV1A = 1000;
+        consensus.nBlockStakeModifierV1A = consensus.nPosStartHeight;
         consensus.nBlockStakeModifierV2 = consensus.V16DeploymentHeight;
         // ATP parameters
-        consensus.ATPStartHeight = std::numeric_limits<int64_t>::max();
+        consensus.ATPStartHeight = consensus.V16DeploymentHeight;
         consensus.WagerrAddrPrefix = "wagerrreg";
         consensus.strTokenManagementKey = "TqMgq4qkw7bGxf6CDhtDfEqzEtWD5C7x8U";
         consensus.nOpGroupNewRequiredConfirmations = 1;
