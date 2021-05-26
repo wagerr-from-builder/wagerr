108ef0ab3fb324b9d1951a1fae50513997636182
celbalrai <80897309+celbalrai@users.noreply.github.com>
2021-05-26 18:23:55 +0200

    Rebrand - additional renaming

--- src/logging.h
+++ src/logging.h
@@ -62,7 +62,7 @@ namespace BCLog {
         QT          = (1 << 19),
         LEVELDB     = (1 << 20),
 
-        //Start Wagerr
+        //Start Dash
         CHAINLOCKS  = ((uint64_t)1 << 32),
         GOBJECT     = ((uint64_t)1 << 33),
         INSTANTSEND = ((uint64_t)1 << 34),
@@ -75,7 +75,7 @@ namespace BCLog {
         COINJOIN    = ((uint64_t)1 << 41),
         SPORK       = ((uint64_t)1 << 42),
         NETCONN     = ((uint64_t)1 << 43),
-        //End Wagerr
+        //End Dash
 
         //Start Wagerr
         ZEROCOIN    = ((uint64_t)1 << 61),
