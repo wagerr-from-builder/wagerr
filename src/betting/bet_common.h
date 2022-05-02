a99e5a241b1113c87ac5dfb70c1858fc5c4306d2
wagerrck <104495875+wagerrck@users.noreply.github.com>
2022-05-02 10:42:49 +0100

    Update pubkey

--- src/betting/bet_common.h
+++ src/betting/bet_common.h
@@ -165,6 +165,13 @@ class CBetOut : public CTxOut {
         return CTxOut::IsEmpty() && nEventId == 0;
     }
 
+    inline bool operator==(const CBetOut& rhs) const { return CompareTo(rhs) == 0; }
+    inline bool operator!=(const CBetOut& rhs) const { return CompareTo(rhs) != 0; }
+    inline bool operator<=(const CBetOut& rhs) const { return CompareTo(rhs) <= 0; }
+    inline bool operator>=(const CBetOut& rhs) const { return CompareTo(rhs) >= 0; }
+    inline bool operator<(const CBetOut& rhs) const { return CompareTo(rhs) < 0; }
+    inline bool operator>(const CBetOut& rhs) const { return CompareTo(rhs) > 0; }
+
     inline int CompareTo(const CBetOut& rhs) const
     {
         if (nValue < rhs.nValue)
@@ -181,13 +188,6 @@ class CBetOut : public CTxOut {
             return 1;
         return 0;
     }
-
-    inline bool operator==(const CBetOut& rhs) const { return CompareTo(rhs) == 0; }
-    inline bool operator!=(const CBetOut& rhs) const { return CompareTo(rhs) != 0; }
-    inline bool operator<=(const CBetOut& rhs) const { return CompareTo(rhs) <= 0; }
-    inline bool operator>=(const CBetOut& rhs) const { return CompareTo(rhs) >= 0; }
-    inline bool operator<(const CBetOut& rhs) const { return CompareTo(rhs) < 0; }
-    inline bool operator>(const CBetOut& rhs) const { return CompareTo(rhs) > 0; }
 };
 
 /** Ensures a TX has come from an OMNO wallet. **/
