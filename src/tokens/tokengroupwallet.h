bytzck/BYTZ

Token implementation - ATP specific part

src/tokens/tokengroupwallet.h
@@ -1,0 +1,57 @@
+// Copyright (c) 2016-2018 The Bitcoin Unlimited developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef TOKEN_GROUP_RPC_H
+#define TOKEN_GROUP_RPC_H
+
+#include "chainparams.h"
+#include "consensus/tokengroups.h"
+#include "wallet/wallet.h"
+#include <unordered_map>
+
+// Number of satoshis we will put into a grouped output
+static const CAmount GROUPED_SATOSHI_AMT = 1;
+
+// Pass a group and a destination address (or CNoDestination) to get the balance of all outputs in the group
+// or all outputs in that group and on that destination address.
+CAmount GetGroupBalance(const CTokenGroupID &grpID, const CTxDestination &dest, const CWallet *wallet);
+
+// Returns a mapping of groupID->balance
+void GetAllGroupBalances(const CWallet *wallet, std::unordered_map<CTokenGroupID, CAmount> &balances);
+void GetAllGroupBalancesAndAuthorities(const CWallet *wallet, std::unordered_map<CTokenGroupID, CAmount> &balances,
+    std::unordered_map<CTokenGroupID, GroupAuthorityFlags> &authorities);
+void ListAllGroupAuthorities(const CWallet *wallet, std::vector<COutput> &coins);
+void ListGroupAuthorities(const CWallet *wallet, std::vector<COutput> &coins, const CTokenGroupID &grpID);
+void GetGroupBalanceAndAuthorities(CAmount &balance, GroupAuthorityFlags &authorities, const CTokenGroupID &grpID,
+    const CTxDestination &dest, const CWallet *wallet);
+
+void GetGroupCoins(const CWallet *wallet, std::vector<COutput>& coins, CAmount& balance, const CTokenGroupID &grpID, const CTxDestination &dest = CNoDestination());
+void GetGroupAuthority(const CWallet *wallet, std::vector<COutput>& coins, GroupAuthorityFlags flags, const CTokenGroupID &grpID, const CTxDestination &dest = CNoDestination());
+
+// Token group helper functions -- not members because they use objects not available in the consensus lib
+//* Initialize the group id from an address
+CTokenGroupID GetTokenGroup(const CTxDestination &id);
+//* Initialize a group ID from a string representation
+CTokenGroupID GetTokenGroup(const std::string &bytzAddrGrpId, const CChainParams &params = Params());
+
+//* Calculate a group ID based on the provided inputs.  Pass and empty script to opRetTokDesc if there is not
+// going to be an OP_RETURN output in the transaction.
+CTokenGroupID findGroupId(const COutPoint &input, CScript opRetTokDesc, TokenGroupIdFlags flags, uint64_t &nonce);
+
+CAmount GroupCoinSelection(const std::vector<COutput> &coins, CAmount amt, std::vector<COutput> &chosenCoins);
+uint64_t RenewAuthority(const COutput &authority, std::vector<CRecipient> &outputs, CReserveKey &childAuthorityKey);
+
+void ConstructTx(CTransactionRef &txNew, const std::vector<COutput> &chosenCoins, const std::vector<CRecipient> &outputs,
+    CAmount totalAvailable, CAmount totalNeeded, CAmount totalGroupedAvailable, CAmount totalGroupedNeeded,
+    CAmount totalXDMAvailable, CAmount totalXDMNeeded, CTokenGroupID grpID, CWallet *wallet);
+
+void GroupMelt(CTransactionRef &txNew, const CTokenGroupID &grpID, CAmount totalNeeded, CWallet *wallet);
+void GroupSend(CTransactionRef &txNew, const CTokenGroupID &grpID, const std::vector<CRecipient> &outputs,
+    CAmount totalNeeded, CAmount totalXDMNeeded, CWallet *wallet);
+
+
+//* Group script helper function
+CScript GetScriptForDestination(const CTxDestination &dest, const CTokenGroupID &group, const CAmount &amount);
+
+#endif

src/validation.cpp
@@ -53,6 +53,10 @@
 
 #include <statsd_client.h>
 
+#include <tokens/tokendb.h>
+#include <tokens/tokengroupmanager.h>
+#include <tokens/tokengroupwallet.h>
+
 #include <libzerocoin/bignum.h>
 #include <zbytz/accumulators.h>
 #include <zbytz/zbytzchain.h>
@@ -319,6 +323,7 @@ std::unique_ptr<CCoinsViewDB> pcoinsdbview;
 std::unique_ptr<CCoinsViewCache> pcoinsTip;
 std::unique_ptr<CBlockTreeDB> pblocktree;
 std::unique_ptr<CZerocoinDB> zerocoinDB;
+std::unique_ptr<CTokenDB> pTokenDB;
 
 enum class FlushStateMode {
     NONE,
@@ -669,10 +674,31 @@ static bool AcceptToMemoryPoolWorker(const CChainParams& chainparams, CTxMemPool
     // Disallow any OP_GROUP txs from entering the mempool until OP_GROUP is enabled.
     // This ensures that someone won't create an invalid OP_GROUP tx that sits in the mempool until after activation,
     // potentially causing this node to create a bad block.
-    if ((unsigned int)chainActive.Tip()->nHeight < chainparams.GetConsensus().ATPStartHeight) {
-        if (IsAnyTxOutputGrouped(tx))
+    if (IsAnyOutputGrouped(tx)) {
+        if ((unsigned int)chainActive.Tip()->nHeight < chainparams.GetConsensus().ATPStartHeight)
+        {
             return state.DoS(0, false, REJECT_NONSTANDARD, "premature-op_group-tx");
+        } else if (!IsAnyOutputGroupedCreation(tx, TokenGroupIdFlags::MGT_TOKEN) && !tokenGroupManager->ManagementTokensCreated()){
+            for (const CTxOut &txout : tx.vout)
+            {
+                CTokenGroupInfo grp(txout.scriptPubKey);
+                if ((grp.invalid || grp.associatedGroup != NoGroup) && !grp.associatedGroup.hasFlag(TokenGroupIdFlags::MGT_TOKEN)) {
+                    return state.DoS(0, false, REJECT_NONSTANDARD, "op_group-before-mgt-tokens");
+                }
+            }
+        }
     }
+/** TODO (FornaxA): Spork settings
+    //Temporarily disable new token creation during management mode
+    if (GetAdjustedTime() > GetSporkValue(SPORK_10_TOKENGROUP_MAINTENANCE_MODE) && IsAnyOutputGroupedCreation(tx)) {
+        if (IsAnyOutputGroupedCreation(tx, TokenGroupIdFlags::MGT_TOKEN)) {
+            LogPrintf("%s: Management token creation during token group management mode\n", __func__);
+        } else {
+            return state.DoS(0, error("%s : new token creation is not possible during token group management mode",
+                            __func__), REJECT_INVALID, "token-group-management");
+        }
+    }
+*/
 
     // Rather not work on nonstandard transactions (unless -testnet/-regtest)
     std::string reason;
@@ -1413,11 +1439,32 @@ bool CheckInputs(const CTransaction& tx, CValidationState &state, const CCoinsVi
     boost::posix_time::ptime start = boost::posix_time::microsec_clock::local_time();
     if (!tx.IsCoinBase() && !tx.HasZerocoinSpendInputs())
     {
-        if (((unsigned int)chainActive.Tip()->nHeight >= Params().GetConsensus().ATPStartHeight) &&
-            !CheckTokenGroups(tx, state, inputs))
-        {
-            return state.DoS(0, false, REJECT_MALFORMED, "token-group-imbalance", false,
-                strprintf("Token group inputs and outputs do not balance"));
+        if ((unsigned int)chainActive.Tip()->nHeight >= Params().GetConsensus().ATPStartHeight) {
+            std::unordered_map<CTokenGroupID, CTokenGroupBalance> tgMintMeltBalance;
+            CBlockIndex* pindexPrev = mapBlockIndex.find(inputs.GetBestBlock())->second;
+            if (!CheckTokenGroups(tx, state, inputs, tgMintMeltBalance))
+                return state.DoS(0, error("Token group inputs and outputs do not balance"), REJECT_MALFORMED, "token-group-imbalance");
+
+            //Check that all token transactions paid their XDM fees
+            CAmount nXDMFees = 0;
+            if (!fScriptChecks) {
+                if (IsAnyOutputGrouped(tx)) {
+                    if (!tokenGroupManager->CheckXDMFees(tx, tgMintMeltBalance, state, pindexPrev, nXDMFees)) {
+                        return state.DoS(0, error("Token transaction does not pay enough XDM fees"), REJECT_MALFORMED, "token-group-imbalance");
+                    }
+                    if (!tokenGroupManager->ManagementTokensCreated()){
+                        for (const CTxOut &txout : tx.vout)
+                        {
+                            CTokenGroupInfo grp(txout.scriptPubKey);
+                            if ((grp.invalid || grp.associatedGroup != NoGroup) && !grp.associatedGroup.hasFlag(TokenGroupIdFlags::MGT_TOKEN)) {
+                                return state.DoS(0, false, REJECT_NONSTANDARD, "op_group-before-mgt-tokens");
+                            }
+                        }
+                    }
+                }
+            } else {
+                LogPrint(BCLog::TOKEN, "%s - XDM fee payment check skipped on sync\n", __func__);
+            }
         }
 
         if (pvChecks)
@@ -1624,6 +1671,8 @@ int ApplyTxInUndo(Coin&& undo, CCoinsViewCache& view, const COutPoint& out)
  *  When FAILED is returned, view is left in an indeterminate state. */
 DisconnectResult CChainState::DisconnectBlock(const CBlock& block, const CBlockIndex* pindex, CCoinsViewCache& view)
 {
+    std::vector<CTokenGroupID> toRemoveTokenGroupIDs;
+
     bool fDIP0003Active = pindex->nHeight >= Params().GetConsensus().DIP0003Height;
     if (fDIP0003Active && !evoDb->VerifyBestBlock(pindex->GetBlockHash())) {
         // Nodes that upgraded after DIP3 activation will have to reindex to ensure evodb consistency
@@ -1761,10 +1810,19 @@ DisconnectResult CChainState::DisconnectBlock(const CBlock& block, const CBlockI
                 }
 
             }
+            if (IsAnyOutputGroupedCreation(tx)) {
+                CTokenGroupID toRemoveTokenGroupID;
+                if (tokenGroupManager->RemoveTokenGroup(tx, toRemoveTokenGroupID))
+                    toRemoveTokenGroupIDs.push_back(toRemoveTokenGroupID);
+            }
             // At this point, all of txundo.vprevout should have been moved out.
         }
     }
 
+    if (!pTokenDB->EraseTokenGroupBatch(toRemoveTokenGroupIDs)) {
+        AbortNode("Failed to erase token group creations");
+        return DISCONNECT_FAILED;
+    }
 
     if (fSpentIndex) {
         if (!pblocktree->UpdateSpentIndex(spentIndex)) {
@@ -2167,6 +2225,12 @@ bool CChainState::ConnectBlock(const CBlock& block, CValidationState& state, CBl
     //! Zerocoin
     std::vector<std::pair<libzerocoin::CoinSpend, uint256> > vSpends;
     std::vector<std::pair<libzerocoin::PublicCoin, uint256> > vMints;
+    //! ATP
+    std::vector<CTokenGroupCreation> newTokenGroups;
+    unsigned int nXDMCountInBlock = 0;
+    unsigned int nMagicCountInBlock = 0;
+    CAmount nXDMMint = 0;
+    CAmount nMagicMint = 0;
 
     std::vector<PrecomputedTransactionData> txdata;
     txdata.reserve(block.vtx.size()); // Required so that pointers to individual PrecomputedTransactionData don't get invalidated
@@ -2257,6 +2321,29 @@ bool CChainState::ConnectBlock(const CBlock& block, CValidationState& state, CBl
                 }
 
             }
+            if (IsAnyOutputGroupedCreation(*tx)) {
+                if (pindex->nHeight < chainparams.GetConsensus().ATPStartHeight) {
+                    return state.DoS(0, false, REJECT_NONSTANDARD, "premature-op_group-tx");
+                }
+                //Disable new token creation during management mode
+/*
+                if (block.nTime > GetSporkValue(SPORK_10_TOKENGROUP_MAINTENANCE_MODE) && !IsInitialBlockDownload()) {
+                    if (IsAnyOutputGroupedCreation(tx, TokenGroupIdFlags::MGT_TOKEN)) {
+                        LogPrintf("%s: Management token creation during token group management mode\n", __func__);
+                    } else {
+                        return state.DoS(0, error("%s : new token creation is not possible during token group management mode",
+                                        __func__), REJECT_INVALID, "token-group-management");
+                    }
+                }
+*/
+                CTokenGroupCreation newTokenGroupCreation;
+                if (CreateTokenGroup(tx, newTokenGroupCreation)) {
+                    newTokenGroups.push_back(newTokenGroupCreation);
+                } else {
+                    return state.Invalid(false, REJECT_INVALID, "bad OP_GROUP");
+                }
+            }
+
             // Check that zBYTZ mints are not already known
             if (tx->HasZerocoinMintOutputs()) {
                 for (auto& out : tx->vout) {
@@ -2409,6 +2496,14 @@ bool CChainState::ConnectBlock(const CBlock& block, CValidationState& state, CBl
 
     // END DASH
 
+    //Track zBYTZ money supply in the block index
+    if (!UpdateZBYTZSupply(block, pindex, fJustCheck))
+        return state.DoS(100, error("%s: Failed to calculate new zBYTZ supply for block=%s height=%d", __func__,
+                                    block.GetHash().GetHex(), pindex->nHeight), REJECT_INVALID);
+
+    // Track XDM money supply in the block index
+    pindex->nXDMTransactions = nXDMCountInBlock;
+
     // Ensure that accumulator checkpoints are valid and in the same state as this instance of the chain
     AccumulatorMap mapAccumulators(Params().Zerocoin_Params(pindex->nHeight < Params().GetConsensus().nBlockZerocoinV2));
     if (!ValidateAccumulatorCheckpoint(block, pindex, mapAccumulators)) {
@@ -2459,7 +2554,14 @@ bool CChainState::ConnectBlock(const CBlock& block, CValidationState& state, CBl
         if (!pblocktree->WriteTimestampIndex(CTimestampIndexKey(pindex->nTime, pindex->GetBlockHash())))
             return AbortNode(state, "Failed to write timestamp index");
 
+    if (!pTokenDB->WriteTokenGroupsBatch(newTokenGroups))
+        return AbortNode(state, "Failed to write token creation data");
+    if (!tokenGroupManager->AddTokenGroups(newTokenGroups)) {
+        return AbortNode(state, "Failed to add token creation data");
+    }
+
     assert(pindex->phashBlock);
+
     // add this block to the view's block chain
     view.SetBestBlock(pindex->GetBlockHash());
 

wagerr-from-builder/wagerr

src/tokens/tokengroupwallet.h
@@ -1,0 +1,57 @@
+// Copyright (c) 2016-2018 The Bitcoin Unlimited developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef TOKEN_GROUP_RPC_H
+#define TOKEN_GROUP_RPC_H
+
+#include "chainparams.h"
+#include "consensus/tokengroups.h"
+#include "wallet/wallet.h"
+#include <unordered_map>
+
+// Number of satoshis we will put into a grouped output
+static const CAmount GROUPED_SATOSHI_AMT = 1;
+
+// Pass a group and a destination address (or CNoDestination) to get the balance of all outputs in the group
+// or all outputs in that group and on that destination address.
+CAmount GetGroupBalance(const CTokenGroupID &grpID, const CTxDestination &dest, const CWallet *wallet);
+
+// Returns a mapping of groupID->balance
+void GetAllGroupBalances(const CWallet *wallet, std::unordered_map<CTokenGroupID, CAmount> &balances);
+void GetAllGroupBalancesAndAuthorities(const CWallet *wallet, std::unordered_map<CTokenGroupID, CAmount> &balances,
+    std::unordered_map<CTokenGroupID, GroupAuthorityFlags> &authorities);
+void ListAllGroupAuthorities(const CWallet *wallet, std::vector<COutput> &coins);
+void ListGroupAuthorities(const CWallet *wallet, std::vector<COutput> &coins, const CTokenGroupID &grpID);
+void GetGroupBalanceAndAuthorities(CAmount &balance, GroupAuthorityFlags &authorities, const CTokenGroupID &grpID,
+    const CTxDestination &dest, const CWallet *wallet);
+
+void GetGroupCoins(const CWallet *wallet, std::vector<COutput>& coins, CAmount& balance, const CTokenGroupID &grpID, const CTxDestination &dest = CNoDestination());
+void GetGroupAuthority(const CWallet *wallet, std::vector<COutput>& coins, GroupAuthorityFlags flags, const CTokenGroupID &grpID, const CTxDestination &dest = CNoDestination());
+
+// Token group helper functions -- not members because they use objects not available in the consensus lib
+//* Initialize the group id from an address
+CTokenGroupID GetTokenGroup(const CTxDestination &id);
+//* Initialize a group ID from a string representation
+CTokenGroupID GetTokenGroup(const std::string &wagerrAddrGrpId, const CChainParams &params = Params());
+
+//* Calculate a group ID based on the provided inputs.  Pass and empty script to opRetTokDesc if there is not
+// going to be an OP_RETURN output in the transaction.
+CTokenGroupID findGroupId(const COutPoint &input, CScript opRetTokDesc, TokenGroupIdFlags flags, uint64_t &nonce);
+
+CAmount GroupCoinSelection(const std::vector<COutput> &coins, CAmount amt, std::vector<COutput> &chosenCoins);
+uint64_t RenewAuthority(const COutput &authority, std::vector<CRecipient> &outputs, CReserveKey &childAuthorityKey);
+
+void ConstructTx(CTransactionRef &txNew, const std::vector<COutput> &chosenCoins, const std::vector<CRecipient> &outputs,
+    CAmount totalAvailable, CAmount totalNeeded, CAmount totalGroupedAvailable, CAmount totalGroupedNeeded,
+    CAmount totalXDMAvailable, CAmount totalXDMNeeded, CTokenGroupID grpID, CWallet *wallet);
+
+void GroupMelt(CTransactionRef &txNew, const CTokenGroupID &grpID, CAmount totalNeeded, CWallet *wallet);
+void GroupSend(CTransactionRef &txNew, const CTokenGroupID &grpID, const std::vector<CRecipient> &outputs,
+    CAmount totalNeeded, CAmount totalXDMNeeded, CWallet *wallet);
+
+
+//* Group script helper function
+CScript GetScriptForDestination(const CTxDestination &dest, const CTokenGroupID &group, const CAmount &amount);
+
+#endif

src/validation.cpp
@@ -53,6 +53,10 @@
 
 #include <statsd_client.h>
 
+#include <tokens/tokendb.h>
+#include <tokens/tokengroupmanager.h>
+#include <tokens/tokengroupwallet.h>
+
 #include <libzerocoin/bignum.h>
 #include <zwgr/accumulators.h>
 #include <zwgr/zwgrchain.h>
@@ -319,6 +323,7 @@ std::unique_ptr<CCoinsViewDB> pcoinsdbview;
 std::unique_ptr<CCoinsViewCache> pcoinsTip;
 std::unique_ptr<CBlockTreeDB> pblocktree;
 std::unique_ptr<CZerocoinDB> zerocoinDB;
+std::unique_ptr<CTokenDB> pTokenDB;
 
 enum class FlushStateMode {
     NONE,
@@ -669,10 +674,31 @@ static bool AcceptToMemoryPoolWorker(const CChainParams& chainparams, CTxMemPool
     // Disallow any OP_GROUP txs from entering the mempool until OP_GROUP is enabled.
     // This ensures that someone won't create an invalid OP_GROUP tx that sits in the mempool until after activation,
     // potentially causing this node to create a bad block.
-    if ((unsigned int)chainActive.Tip()->nHeight < chainparams.GetConsensus().ATPStartHeight) {
-        if (IsAnyTxOutputGrouped(tx))
+    if (IsAnyOutputGrouped(tx)) {
+        if ((unsigned int)chainActive.Tip()->nHeight < chainparams.GetConsensus().ATPStartHeight)
+        {
             return state.DoS(0, false, REJECT_NONSTANDARD, "premature-op_group-tx");
+        } else if (!IsAnyOutputGroupedCreation(tx, TokenGroupIdFlags::MGT_TOKEN) && !tokenGroupManager->ManagementTokensCreated()){
+            for (const CTxOut &txout : tx.vout)
+            {
+                CTokenGroupInfo grp(txout.scriptPubKey);
+                if ((grp.invalid || grp.associatedGroup != NoGroup) && !grp.associatedGroup.hasFlag(TokenGroupIdFlags::MGT_TOKEN)) {
+                    return state.DoS(0, false, REJECT_NONSTANDARD, "op_group-before-mgt-tokens");
+                }
+            }
+        }
     }
+/** TODO (FornaxA): Spork settings
+    //Temporarily disable new token creation during management mode
+    if (GetAdjustedTime() > GetSporkValue(SPORK_10_TOKENGROUP_MAINTENANCE_MODE) && IsAnyOutputGroupedCreation(tx)) {
+        if (IsAnyOutputGroupedCreation(tx, TokenGroupIdFlags::MGT_TOKEN)) {
+            LogPrintf("%s: Management token creation during token group management mode\n", __func__);
+        } else {
+            return state.DoS(0, error("%s : new token creation is not possible during token group management mode",
+                            __func__), REJECT_INVALID, "token-group-management");
+        }
+    }
+*/
 
     // Rather not work on nonstandard transactions (unless -testnet/-regtest)
     std::string reason;
@@ -1413,11 +1439,32 @@ bool CheckInputs(const CTransaction& tx, CValidationState &state, const CCoinsVi
     boost::posix_time::ptime start = boost::posix_time::microsec_clock::local_time();
     if (!tx.IsCoinBase() && !tx.HasZerocoinSpendInputs())
     {
-        if (((unsigned int)chainActive.Tip()->nHeight >= Params().GetConsensus().ATPStartHeight) &&
-            !CheckTokenGroups(tx, state, inputs))
-        {
-            return state.DoS(0, false, REJECT_MALFORMED, "token-group-imbalance", false,
-                strprintf("Token group inputs and outputs do not balance"));
+        if ((unsigned int)chainActive.Tip()->nHeight >= Params().GetConsensus().ATPStartHeight) {
+            std::unordered_map<CTokenGroupID, CTokenGroupBalance> tgMintMeltBalance;
+            CBlockIndex* pindexPrev = mapBlockIndex.find(inputs.GetBestBlock())->second;
+            if (!CheckTokenGroups(tx, state, inputs, tgMintMeltBalance))
+                return state.DoS(0, error("Token group inputs and outputs do not balance"), REJECT_MALFORMED, "token-group-imbalance");
+
+            //Check that all token transactions paid their XDM fees
+            CAmount nXDMFees = 0;
+            if (!fScriptChecks) {
+                if (IsAnyOutputGrouped(tx)) {
+                    if (!tokenGroupManager->CheckXDMFees(tx, tgMintMeltBalance, state, pindexPrev, nXDMFees)) {
+                        return state.DoS(0, error("Token transaction does not pay enough XDM fees"), REJECT_MALFORMED, "token-group-imbalance");
+                    }
+                    if (!tokenGroupManager->ManagementTokensCreated()){
+                        for (const CTxOut &txout : tx.vout)
+                        {
+                            CTokenGroupInfo grp(txout.scriptPubKey);
+                            if ((grp.invalid || grp.associatedGroup != NoGroup) && !grp.associatedGroup.hasFlag(TokenGroupIdFlags::MGT_TOKEN)) {
+                                return state.DoS(0, false, REJECT_NONSTANDARD, "op_group-before-mgt-tokens");
+                            }
+                        }
+                    }
+                }
+            } else {
+                LogPrint(BCLog::TOKEN, "%s - XDM fee payment check skipped on sync\n", __func__);
+            }
         }
 
         if (pvChecks)
@@ -1624,6 +1671,8 @@ int ApplyTxInUndo(Coin&& undo, CCoinsViewCache& view, const COutPoint& out)
  *  When FAILED is returned, view is left in an indeterminate state. */
 DisconnectResult CChainState::DisconnectBlock(const CBlock& block, const CBlockIndex* pindex, CCoinsViewCache& view)
 {
+    std::vector<CTokenGroupID> toRemoveTokenGroupIDs;
+
     bool fDIP0003Active = pindex->nHeight >= Params().GetConsensus().DIP0003Height;
     if (fDIP0003Active && !evoDb->VerifyBestBlock(pindex->GetBlockHash())) {
         // Nodes that upgraded after DIP3 activation will have to reindex to ensure evodb consistency
@@ -1761,10 +1810,19 @@ DisconnectResult CChainState::DisconnectBlock(const CBlock& block, const CBlockI
                 }
 
             }
+            if (IsAnyOutputGroupedCreation(tx)) {
+                CTokenGroupID toRemoveTokenGroupID;
+                if (tokenGroupManager->RemoveTokenGroup(tx, toRemoveTokenGroupID))
+                    toRemoveTokenGroupIDs.push_back(toRemoveTokenGroupID);
+            }
             // At this point, all of txundo.vprevout should have been moved out.
         }
     }
 
+    if (!pTokenDB->EraseTokenGroupBatch(toRemoveTokenGroupIDs)) {
+        AbortNode("Failed to erase token group creations");
+        return DISCONNECT_FAILED;
+    }
 
     if (fSpentIndex) {
         if (!pblocktree->UpdateSpentIndex(spentIndex)) {
@@ -2167,6 +2225,12 @@ bool CChainState::ConnectBlock(const CBlock& block, CValidationState& state, CBl
     //! Zerocoin
     std::vector<std::pair<libzerocoin::CoinSpend, uint256> > vSpends;
     std::vector<std::pair<libzerocoin::PublicCoin, uint256> > vMints;
+    //! ATP
+    std::vector<CTokenGroupCreation> newTokenGroups;
+    unsigned int nXDMCountInBlock = 0;
+    unsigned int nMagicCountInBlock = 0;
+    CAmount nXDMMint = 0;
+    CAmount nMagicMint = 0;
 
     std::vector<PrecomputedTransactionData> txdata;
     txdata.reserve(block.vtx.size()); // Required so that pointers to individual PrecomputedTransactionData don't get invalidated
@@ -2257,6 +2321,29 @@ bool CChainState::ConnectBlock(const CBlock& block, CValidationState& state, CBl
                 }
 
             }
+            if (IsAnyOutputGroupedCreation(*tx)) {
+                if (pindex->nHeight < chainparams.GetConsensus().ATPStartHeight) {
+                    return state.DoS(0, false, REJECT_NONSTANDARD, "premature-op_group-tx");
+                }
+                //Disable new token creation during management mode
+/*
+                if (block.nTime > GetSporkValue(SPORK_10_TOKENGROUP_MAINTENANCE_MODE) && !IsInitialBlockDownload()) {
+                    if (IsAnyOutputGroupedCreation(tx, TokenGroupIdFlags::MGT_TOKEN)) {
+                        LogPrintf("%s: Management token creation during token group management mode\n", __func__);
+                    } else {
+                        return state.DoS(0, error("%s : new token creation is not possible during token group management mode",
+                                        __func__), REJECT_INVALID, "token-group-management");
+                    }
+                }
+*/
+                CTokenGroupCreation newTokenGroupCreation;
+                if (CreateTokenGroup(tx, newTokenGroupCreation)) {
+                    newTokenGroups.push_back(newTokenGroupCreation);
+                } else {
+                    return state.Invalid(false, REJECT_INVALID, "bad OP_GROUP");
+                }
+            }
+
             // Check that zWAGERR mints are not already known
             if (tx->HasZerocoinMintOutputs()) {
                 for (auto& out : tx->vout) {
@@ -2409,6 +2496,14 @@ bool CChainState::ConnectBlock(const CBlock& block, CValidationState& state, CBl
 
     // END DASH
 
+    //Track zWAGERR money supply in the block index
+    if (!UpdateZWGRSupply(block, pindex, fJustCheck))
+        return state.DoS(100, error("%s: Failed to calculate new zWAGERR supply for block=%s height=%d", __func__,
+                                    block.GetHash().GetHex(), pindex->nHeight), REJECT_INVALID);
+
+    // Track XDM money supply in the block index
+    pindex->nXDMTransactions = nXDMCountInBlock;
+
     // Ensure that accumulator checkpoints are valid and in the same state as this instance of the chain
     AccumulatorMap mapAccumulators(Params().Zerocoin_Params(pindex->nHeight < Params().GetConsensus().nBlockZerocoinV2));
     if (!ValidateAccumulatorCheckpoint(block, pindex, mapAccumulators)) {
@@ -2459,7 +2554,14 @@ bool CChainState::ConnectBlock(const CBlock& block, CValidationState& state, CBl
         if (!pblocktree->WriteTimestampIndex(CTimestampIndexKey(pindex->nTime, pindex->GetBlockHash())))
             return AbortNode(state, "Failed to write timestamp index");
 
+    if (!pTokenDB->WriteTokenGroupsBatch(newTokenGroups))
+        return AbortNode(state, "Failed to write token creation data");
+    if (!tokenGroupManager->AddTokenGroups(newTokenGroups)) {
+        return AbortNode(state, "Failed to add token creation data");
+    }
+
     assert(pindex->phashBlock);
+
     // add this block to the view's block chain
     view.SetBestBlock(pindex->GetBlockHash());
 