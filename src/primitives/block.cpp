5ce62672b72caed02ea1d45d6a974a0ec6ee2dd8
wagerrck <104495875+wagerrck@users.noreply.github.com>
2022-05-13 11:30:12 +0100

    28

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -12,7 +12,7 @@
 #include <versionbits.h>
 #include <crypto/common.h>
 
-uint256 CBlockHeader::GetHash() const
+/* uint256 CBlockHeader::GetHash() const
 {
     // CVectorWriter grows vch when necessary
     std::vector<unsigned char> vch(80);
@@ -23,6 +23,23 @@ uint256 CBlockHeader::GetHash() const
     } else {
         return Hash((const char *)vch.data(), (const char *)vch.data() + vch.size());
     }
+} */
+
+uint256 CBlockHeader::GetHash() const
+{
+    // CVectorWriter grows vch when necessary
+    std::vector<unsigned char> vch(80);
+    CVectorWriter ss(SER_GETHASH, PROTOCOL_VERSION, vch, 0);
+    ss << *this;
+    if (nVersion < 4)
+        return HashQuark((const char *)vch.data(), (const char *)vch.data() + vch.size());
+        //return HashQuark(BEGIN(nVersion), END(nNonce));
+
+    if (nVersion < 7)
+        return Hash((const char *)vch.data(), (const char *)vch.data() + vch.size());
+        //return Hash(BEGIN(nVersion), END(nAccumulatorCheckpoint));
+
+    return Hash(BEGIN(nVersion), END(nNonce));
 }
 
 std::string CBlock::ToString() const
