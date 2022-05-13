watto-engineer/dash

Make tests with tokens functional again

test/functional/feature_dip3_deterministicmns.py
@@ -15,6 +15,8 @@ from test_framework.test_framework import BytzTestFramework
 from test_framework.util import *
 from time import sleep
 
+BYTZ_AUTH_ADDR = "TqMgq4qkw7bGxf6CDhtDfEqzEtWD5C7x8U"
+
 class Masternode(object):
     pass
 
@@ -77,6 +79,8 @@ class DIP3Test(BytzTestFramework):
         self.nodes[0].generate(1)
         MGTGroup_ID=MGT['groupID']
         self.nodes[0].minttoken(MGTGroup_ID, MGTAddr, '25')
+        self.nodes[0].sendtoaddress(BYTZ_AUTH_ADDR, 10)
+        self.nodes[0].generate(1)
         GVT=self.nodes[0].configuremanagementtoken("GVT", "GuardianValidator", "0", "https://www.google.com", "0", GVTBLS["public"], "true", "true")
         self.nodes[0].generate(1)
         GVTGroup_ID=GVT['groupID']

test/functional/feature_startmn.py
@@ -10,6 +10,7 @@ from test_framework.test_framework import BytzTestFramework
 from test_framework.util import *
 from test_framework.blocktools import *
 
+BYTZ_AUTH_ADDR = "TqMgq4qkw7bGxf6CDhtDfEqzEtWD5C7x8U"
 
 class WalletTest(BytzTestFramework):
     def set_test_params(self):
@@ -67,6 +68,8 @@ class WalletTest(BytzTestFramework):
         gvtBLSKey = self.nodes[0].bls("generate")
         self.log.info("gvtBLSKey:")
         self.log.info(gvtBLSKey)
+        self.nodes[0].sendtoaddress(BYTZ_AUTH_ADDR, 10)
+        self.nodes[0].generate(1)
         gvtConfig = self.nodes[0].configuremanagementtoken("GVT", "GuardianValidatorToken", "0", "https://www.google.com", "4f92d91db24bb0b8ca24a2ec86c4b012ccdc4b2e9d659c2079f5cc358413a765", gvtBLSKey['public'], "true", "true")
         self.nodes[0].generate(1)
         self.log.info("gvtConfig:")

test/functional/rpc_token_test_pt1.py
@@ -54,18 +54,28 @@ class TokenTest (BitcoinTestFramework):
         self.log.info("MGT %s" % MGT)
         MGTGroup_ID=MGT['groupID']
         self.nodes[0].minttoken(MGTGroup_ID, MGTAddr, '82')
+        self.nodes[0].sendtoaddress(BYTZ_AUTH_ADDR, 10)
+        self.nodes[0].generate(1)
         GVT=self.nodes[0].configuremanagementtoken("GVT", "GuardianValidator","0",  "https://www.google.com", "0", GVTBLS["public"], "false", "true")
         self.nodes[0].generate(1)
         self.log.info("GVT %s" % GVT)
         GVTGroup_ID=GVT['groupID']
         self.nodes[0].minttoken(GVTGroup_ID, GVTAddr, '43')
         mintaddr=self.nodes[0].getnewaddress()
+        self.nodes[0].sendtoaddress(BYTZ_AUTH_ADDR, 10)
+        self.nodes[0].generate(1)
         self.nodes[0].minttoken(MGTGroup_ID, mintaddr, 500)
+        self.nodes[0].generate(1)
+        self.nodes[0].sendtoaddress(BYTZ_AUTH_ADDR, 10)
         self.nodes[0].generate(1)
         XBYTZTok=self.nodes[0].configuremanagementtoken("XBYTZ", "ExtraBytz", "0", "https://github.com/bytzcurrency/ATP-descriptions/blob/master/BYTZ-testnet-XBYTZ.json","f5125a90bde180ef073ce1109376d977f5cbddb5582643c81424cc6cc842babd",XBYTZBLS["public"], "true", "true")
         XBYTZGroup_ID=XBYTZTok['groupID']
+        self.nodes[0].sendtoaddress(BYTZ_AUTH_ADDR, 10)
+        self.nodes[0].generate(1)
         PARTTok=self.nodes[0].configuremanagementtoken("PART", "PartBytz", "0", "https://github.com/bytzcurrency/ATP-descriptions/blob/master/BYTZ-testnet-PART.json", "b0425ee4ba234099970c53c28288da749e2a1afc0f49856f4cab82b37f72f6a5",PARTBLS["public"], "true", "true")
         PARTGroup_ID=PARTTok['groupID']
+        self.nodes[0].sendtoaddress(BYTZ_AUTH_ADDR, 10)
+        self.nodes[0].generate(1)
         LIVETok=self.nodes[0].configuremanagementtoken("LIVE", "LiveBytz", "13", "https://github.com/bytzcurrency/ATP-descriptions/blob/master/BYTZ-testnet-LIVE.json", "6de2409add060ec4ef03d61c0966dc46508ed3498e202e9459e492a372ddccf5", LiveBLS["public"], "true", "true")
         LIVEGroup_ID=LIVETok['groupID']
         self.nodes[0].generate(1)

test/functional/test_framework/test_framework.py
@@ -55,6 +55,8 @@ from .util import (
     get_chain_folder,
 )
 
+BYTZ_AUTH_ADDR = "TqMgq4qkw7bGxf6CDhtDfEqzEtWD5C7x8U"
+
 class TestStatus(Enum):
     PASSED = 1
     FAILED = 2
@@ -597,6 +599,8 @@ class BytzTestFramework(BitcoinTestFramework):
         self.nodes[0].generate(1)
         MGTGroup_ID=MGT['groupID']
         self.nodes[0].minttoken(MGTGroup_ID, MGTAddr, '25')
+        self.nodes[0].sendtoaddress(BYTZ_AUTH_ADDR, 10)
+        self.nodes[0].generate(1)
         GVT=self.nodes[0].configuremanagementtoken("GVT", "GuardianValidator", "0", "https://www.google.com", "4f92d91db24bb0b8ca24a2ec86c4b012ccdc4b2e9d659c2079f5cc358413a765", GVTBLS["public"], "true", "true")
         self.nodes[0].generate(1)
         GVTGroup_ID=GVT['groupID']

wagerr-from-builder/wagerr

test/functional/feature_dip3_deterministicmns.py
@@ -15,6 +15,8 @@ from test_framework.test_framework import WagerrTestFramework
 from test_framework.util import *
 from time import sleep
 
+WAGERR_AUTH_ADDR = "TqMgq4qkw7bGxf6CDhtDfEqzEtWD5C7x8U"
+
 class Masternode(object):
     pass
 
@@ -77,6 +79,8 @@ class DIP3Test(WagerrTestFramework):
         self.nodes[0].generate(1)
         MGTGroup_ID=MGT['groupID']
         self.nodes[0].minttoken(MGTGroup_ID, MGTAddr, '25')
+        self.nodes[0].sendtoaddress(WAGERR_AUTH_ADDR, 10)
+        self.nodes[0].generate(1)
         GVT=self.nodes[0].configuremanagementtoken("GVT", "GuardianValidator", "0", "https://www.google.com", "0", GVTBLS["public"], "true", "true")
         self.nodes[0].generate(1)
         GVTGroup_ID=GVT['groupID']

test/functional/feature_startmn.py
@@ -10,6 +10,7 @@ from test_framework.test_framework import WagerrTestFramework
 from test_framework.util import *
 from test_framework.blocktools import *
 
+WAGERR_AUTH_ADDR = "TqMgq4qkw7bGxf6CDhtDfEqzEtWD5C7x8U"
 
 class WalletTest(WagerrTestFramework):
     def set_test_params(self):
@@ -67,6 +68,8 @@ class WalletTest(WagerrTestFramework):
         gvtBLSKey = self.nodes[0].bls("generate")
         self.log.info("gvtBLSKey:")
         self.log.info(gvtBLSKey)
+        self.nodes[0].sendtoaddress(WAGERR_AUTH_ADDR, 10)
+        self.nodes[0].generate(1)
         gvtConfig = self.nodes[0].configuremanagementtoken("GVT", "GuardianValidatorToken", "0", "https://www.google.com", "4f92d91db24bb0b8ca24a2ec86c4b012ccdc4b2e9d659c2079f5cc358413a765", gvtBLSKey['public'], "true", "true")
         self.nodes[0].generate(1)
         self.log.info("gvtConfig:")

test/functional/rpc_token_test_pt1.py
@@ -54,18 +54,28 @@ class TokenTest (BitcoinTestFramework):
         self.log.info("MGT %s" % MGT)
         MGTGroup_ID=MGT['groupID']
         self.nodes[0].minttoken(MGTGroup_ID, MGTAddr, '82')
+        self.nodes[0].sendtoaddress(WAGERR_AUTH_ADDR, 10)
+        self.nodes[0].generate(1)
         GVT=self.nodes[0].configuremanagementtoken("GVT", "GuardianValidator","0",  "https://www.google.com", "0", GVTBLS["public"], "false", "true")
         self.nodes[0].generate(1)
         self.log.info("GVT %s" % GVT)
         GVTGroup_ID=GVT['groupID']
         self.nodes[0].minttoken(GVTGroup_ID, GVTAddr, '43')
         mintaddr=self.nodes[0].getnewaddress()
+        self.nodes[0].sendtoaddress(WAGERR_AUTH_ADDR, 10)
+        self.nodes[0].generate(1)
         self.nodes[0].minttoken(MGTGroup_ID, mintaddr, 500)
+        self.nodes[0].generate(1)
+        self.nodes[0].sendtoaddress(WAGERR_AUTH_ADDR, 10)
         self.nodes[0].generate(1)
         XWAGERRTok=self.nodes[0].configuremanagementtoken("XWAGERR", "ExtraWagerr", "0", "https://github.com/wagerr/ATP-descriptions/blob/master/WAGERR-testnet-XWAGERR.json","f5125a90bde180ef073ce1109376d977f5cbddb5582643c81424cc6cc842babd",XWAGERRBLS["public"], "true", "true")
         XWAGERRGroup_ID=XWAGERRTok['groupID']
+        self.nodes[0].sendtoaddress(WAGERR_AUTH_ADDR, 10)
+        self.nodes[0].generate(1)
         PARTTok=self.nodes[0].configuremanagementtoken("PART", "PartWagerr", "0", "https://github.com/wagerr/ATP-descriptions/blob/master/WAGERR-testnet-PART.json", "b0425ee4ba234099970c53c28288da749e2a1afc0f49856f4cab82b37f72f6a5",PARTBLS["public"], "true", "true")
         PARTGroup_ID=PARTTok['groupID']
+        self.nodes[0].sendtoaddress(WAGERR_AUTH_ADDR, 10)
+        self.nodes[0].generate(1)
         LIVETok=self.nodes[0].configuremanagementtoken("LIVE", "LiveWagerr", "13", "https://github.com/wagerr/ATP-descriptions/blob/master/WAGERR-testnet-LIVE.json", "6de2409add060ec4ef03d61c0966dc46508ed3498e202e9459e492a372ddccf5", LiveBLS["public"], "true", "true")
         LIVEGroup_ID=LIVETok['groupID']
         self.nodes[0].generate(1)

test/functional/test_framework/test_framework.py
@@ -55,6 +55,8 @@ from .util import (
     get_chain_folder,
 )
 
+WAGERR_AUTH_ADDR = "TqMgq4qkw7bGxf6CDhtDfEqzEtWD5C7x8U"
+
 class TestStatus(Enum):
     PASSED = 1
     FAILED = 2
@@ -597,6 +599,8 @@ class WagerrTestFramework(BitcoinTestFramework):
         self.nodes[0].generate(1)
         MGTGroup_ID=MGT['groupID']
         self.nodes[0].minttoken(MGTGroup_ID, MGTAddr, '25')
+        self.nodes[0].sendtoaddress(WAGERR_AUTH_ADDR, 10)
+        self.nodes[0].generate(1)
         GVT=self.nodes[0].configuremanagementtoken("GVT", "GuardianValidator", "0", "https://www.google.com", "4f92d91db24bb0b8ca24a2ec86c4b012ccdc4b2e9d659c2079f5cc358413a765", GVTBLS["public"], "true", "true")
         self.nodes[0].generate(1)
         GVTGroup_ID=GVT['groupID']