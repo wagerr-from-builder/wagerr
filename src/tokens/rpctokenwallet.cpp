watto-engineer/dash

Add token document hashing, signing and verification

src/tokens/rpctokenwallet.cpp
@@ -12,6 +12,7 @@
 #include "rpc/protocol.h"
 #include "rpc/server.h"
 #include "script/tokengroup.h"
+#include "tokens/tokengroupdocument.h"
 #include "tokens/tokengroupmanager.h"
 #include "utilmoneystr.h"
 #include "utilstrencodings.h"
@@ -1909,6 +1910,73 @@ UniValue listunspenttokens(const JSONRPCRequest& request)
     return results;
 }
 
+UniValue signrawtokendocument(const JSONRPCRequest& request)
+{
+    std::shared_ptr<CWallet> const wallet = GetWalletForJSONRPCRequest(request);
+    CWallet* const pwallet = wallet.get();
+    if (!EnsureWalletIsAvailable(pwallet, request.fHelp)) {
+        return NullUniValue;
+    }
+
+    if (request.fHelp || request.params.size() < 2 || request.params.size() > 3) {
+        throw std::runtime_error(
+                "signrawtokendocument \"data\" \"address\" ( \"verbose\" )\n"
+                "\nSigns the raw token document using the supplied address.\n"
+                "\nArguments:\n"
+                "1. \"data\"           (hex, required) The (unsigned) serialized token document.\n"
+                "2. \"address\"        (string, required) The address used to sign the token document.\n"
+                "3. \"verbose\"        (bool, optional,default=false) Output the json encoded specification instead of the serialized data.\n"
+        );
+    }
+
+    LOCK2(cs_main, pwallet->cs_wallet);
+    RPCTypeCheck(request.params, {UniValue::VSTR, UniValue::VSTR});
+
+    bool fVerbose = false;
+    if (request.params.size() > 2) {
+        std::string sVerbose;
+        std::string p = request.params[2].get_str();
+        std::transform(p.begin(), p.end(), std::back_inserter(sVerbose), ::tolower);
+        fVerbose = (sVerbose == "true");
+    }
+
+    EnsureWalletIsUnlocked(pwallet);
+
+    std::string strHexDescription = request.params[0].get_str();
+    std::string strAddress = request.params[1].get_str();
+    CTxDestination dest = DecodeDestination(strAddress);
+    if (!IsValidDestination(dest)) {
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Bytz address");
+    }
+    const CKeyID *keyID = boost::get<CKeyID>(&dest);
+    if (!keyID) {
+        throw JSONRPCError(RPC_TYPE_ERROR, "Address does not refer to a key");
+    }
+    CKey vchSecret;
+    if (!pwallet->GetKey(*keyID, vchSecret)) {
+        throw JSONRPCError(RPC_WALLET_ERROR, "Private key for address " + strAddress + " is not known");
+    }
+
+    CDataStream ssTGDocument(ParseHexV(request.params[0], "data"), SER_NETWORK, PROTOCOL_VERSION);
+    CTokenGroupDocument tgDocument;
+    ssTGDocument >> tgDocument;
+
+    if (!tgDocument.Sign(vchSecret)) {
+        throw JSONRPCError(RPC_WALLET_ERROR, "Unable to sign document");
+    }
+
+    if (fVerbose) {
+        UniValue ret(UniValue::VOBJ);
+        tgDocument.ToJson(ret);
+        return ret;
+    }
+    CDataStream ssTGDocumentOut(SER_NETWORK, PROTOCOL_VERSION);
+    ssTGDocumentOut << tgDocument;
+    std::string strData = HexStr(ssTGDocumentOut.begin(), ssTGDocumentOut.end());
+
+    return strData;
+}
+
 static const CRPCCommand commands[] =
 { //  category              name                        actor (function)            argNames
   //  --------------------- --------------------------  --------------------------  ----------
@@ -1919,6 +1987,7 @@ static const CRPCCommand commands[] =
     { "tokens",             "sendtoken",                &sendtoken,                 {} },
     { "tokens",             "configuretoken",           &configuretoken,            {} },
     { "tokens",             "configuremanagementtoken", &configuremanagementtoken,  {} },
+    { "tokens",             "signrawtokendocument",     &signrawtokendocument,      {"data","address","verbose"} },
     { "tokens",             "createtokenauthorities",   &createtokenauthorities,    {} },
     { "tokens",             "listtokenauthorities",     &listtokenauthorities,      {} },
     { "tokens",             "droptokenauthorities",     &droptokenauthorities,      {} },

src/tokens/tokengroupdocument.cpp
@@ -1,0 +1,112 @@
+// Copyright (c) 2019 The ION Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "tokens/tokengroupdocument.h"
+#include "util.h"
+#include <utilstrencodings.h>
+#include <messagesigner.h>
+#include <univalue.h>
+
+/*
+{
+  "atp": "1.0",
+  "data": {
+    "ticker": "MGT",
+    "name": "Management Token",
+    "chain": "BYTZ.testnet",
+    "summary": "The MGT token is a tokenized management key on the BYTZ blockchain with special authorities necessary for: (1) the construction of a token system with coherent economic incentives; (2) the inception of Nucleus Tokens (special tokens that have interrelated monetary policies); and (3) the distribution of rewards that sustain this system of cryptographic tokens on the blockchain.",
+    "description": "The Atomic Token Protocol (ATP) introduces cross-coin and cross-token policy. BYTZ utilizes ATP for its reward system and rights structure. Management Token (MGT), Guardian Validator Token (GVT), and Guardian Validators all participate in an interconnected managent system, and are considered the Nucleus Tokens. The MGT token itself is a tokenized management key with special authorities needed for token inception on the blockchain. The MGT token continues to play a role in the management of and access to special features.",
+    "creator": "The BYTZ Core Developers",
+    "contact": {
+      "url": "https://github.com/bytzcurrency/bytz"
+    }
+  },
+  "hash": "467723bc0c114a4459e05d8d422ee6766dede23019190a68005290cc053a7e36",
+  "signature": ""
+}
+*/
+void CTokenGroupDocument::ToJson(UniValue& obj) const
+{
+    obj.clear();
+    obj.setObject();
+    obj.pushKV("atp", nVersion);
+    UniValue dataObj(UniValue::VOBJ);
+    dataObj.pushKV("ticker", strTicker);
+    dataObj.pushKV("name", strName);
+    dataObj.pushKV("chain", strChain);
+    dataObj.pushKV("summary", strSummary);
+    dataObj.pushKV("description", strDescription);
+    dataObj.pushKV("creator", strCreator);
+    UniValue dataContactObj(UniValue::VOBJ);
+    dataContactObj.pushKV("url", strContactURL);
+    dataContactObj.pushKV("email", strContactEmail);
+    dataObj.pushKV("contact", dataContactObj);
+    obj.pushKV("data", dataObj);
+    obj.pushKV("hash", GetSignatureHash().GetHex());
+    obj.pushKV("signature", HexStr(vchSig));
+}
+
+
+uint256 CTokenGroupDocument::GetHash() const
+{
+    return SerializeHash(*this);
+}
+
+uint256 CTokenGroupDocument::GetSignatureHash() const
+{
+    return GetHash();
+}
+
+bool CTokenGroupDocument::Sign(const CKey& key)
+{
+    if (!key.IsValid()) {
+        LogPrintf("CTokenGroupDocument::Sign -- signing key is not valid\n");
+        return false;
+    }
+
+    CKeyID pubKeyId = key.GetPubKey().GetID();
+    std::string strError = "";
+
+    uint256 hash = GetSignatureHash();
+    if (!CHashSigner::SignHash(hash, key, vchSig)) {
+        LogPrintf("CTokenGroupDocument::Sign -- SignMessage() failed\n");
+        return false;
+    }
+
+
+    if(!CHashSigner::VerifyHash(hash, pubKeyId, vchSig, strError)) {
+        LogPrintf("CTokenGroupDocument::Sign -- VerifyMessage() failed, error: %s\n", strError);
+        return false;
+    }
+
+    return true;
+}
+
+bool CTokenGroupDocument::CheckSignature(const CKeyID& pubKeyId) const
+{
+    std::string strError = "";
+
+    uint256 hash = GetSignatureHash();
+
+    if(!CHashSigner::VerifyHash(hash, pubKeyId, vchSig, strError)) {
+        LogPrintf("CTokenGroupDocument::Sign -- VerifyMessage() failed, error: %s\n", strError);
+        return false;
+    }
+
+    return true;
+}
+
+bool CTokenGroupDocument::GetSignerKeyID(CKeyID &retKeyidSporkSigner) const
+{
+    CPubKey pubkeyFromSig;
+    uint256 hash = GetSignatureHash();
+    CHashWriter ss(SER_GETHASH, 0);
+    if (!pubkeyFromSig.RecoverCompact(hash, vchSig)) {
+        return false;
+    }
+
+    retKeyidSporkSigner = pubkeyFromSig.GetID();
+    return true;
+}
+

src/tokens/tokengroupdocument.h
@@ -1,0 +1,134 @@
+// Copyright (c) 2019 The ION Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef TOKEN_GROUP_DOCUMENT
+#define TOKEN_GROUP_DOCUMENT
+
+#include <key.h>
+
+class UniValue;
+
+class CTokenGroupDocument
+{
+public:
+    static const uint16_t CURRENT_VERSION = 1;
+
+public:
+    uint16_t nVersion{CURRENT_VERSION};
+
+/*
+{
+  "atp": "1.0",
+  "data": {
+    "ticker": "MGT",
+    "name": "Management Token",
+    "chain": "BYTZ.testnet",
+    "summary": "The MGT token is a tokenized management key on the BYTZ blockchain with special authorities necessary for: (1) the construction of a token system with coherent economic incentives; (2) the inception of Nucleus Tokens (special tokens that have interrelated monetary policies); and (3) the distribution of rewards that sustain this system of cryptographic tokens on the blockchain.",
+    "description": "The Atomic Token Protocol (ATP) introduces cross-coin and cross-token policy. BYTZ utilizes ATP for its reward system and rights structure. Management Token (MGT), Guardian Validator Token (GVT), and Guardian Validators all participate in an interconnected managent system, and are considered the Nucleus Tokens. The MGT token itself is a tokenized management key with special authorities needed for token inception on the blockchain. The MGT token continues to play a role in the management of and access to special features.",
+    "creator": "The BYTZ Core Developers",
+    "contact": {
+      "url": "https://github.com/bytzcurrency/bytz"
+    }
+  },
+  "hash": "467723bc0c114a4459e05d8d422ee6766dede23019190a68005290cc053a7e36",
+  "signature": ""
+}
+*/
+    // data
+    std::string strTicker;
+    std::string strName;
+    std::string strChain;
+    std::string strSummary;
+    std::string strDescription;
+    std::string strCreator;
+    std::string strContactURL;
+    std::string strContactEmail;
+
+    std::vector<unsigned char> vchSig;
+
+    CTokenGroupDocument() {
+        SetNull();
+    };
+    CTokenGroupDocument(std::string strTicker, std::string strName, std::string strChain, std::string strSummary,
+        std::string strDescription, std::string strCreator, std::string strContactURL, std::string strContactEmail) :
+        strTicker(strTicker), strName(strName), strChain(strChain), strSummary(strSummary), strDescription(strDescription),
+        strCreator(strCreator), strContactURL(strContactURL), strContactEmail(strContactEmail) {};
+
+    void SetNull() {
+        strTicker = "";
+        strName = "";
+        strChain = "";
+        strSummary = "";
+        strDescription = "";
+        strCreator = "";
+        strContactURL = "";
+        strContactEmail = "";
+        vchSig.clear();
+    }
+
+    /**
+     * GetHash returns the double-sha256 hash of the serialized item.
+     */
+    uint256 GetHash() const;
+
+    /**
+     * GetSignatureHash returns the hash of the serialized item
+     * without the signature included. The intent of this method is to get the
+     * hash to be signed.
+     */
+    uint256 GetSignatureHash() const;
+
+    /**
+     * Sign will sign the item with the given key.
+     */
+    bool Sign(const CKey& key);
+
+    /**
+     * CheckSignature will ensure the item signature matches the provided public
+     * key hash.
+     */
+    bool CheckSignature(const CKeyID& pubKeyId) const;
+
+    /**
+     * GetSignerKeyID is used to recover the address of the key used to
+     * sign this item.
+     */
+    bool GetSignerKeyID(CKeyID& retKeyidSporkSigner) const;
+
+    // FUNCTIONS FOR DEALING WITH DATA STRING
+
+    std::string GetDataAsHexString() const;
+    std::string GetDataAsPlainString() const;
+
+    // SERIALIZER
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action)
+    {
+        if (!(s.GetType() & SER_GETHASH)) {
+            READWRITE(nVersion);
+        }
+        READWRITE(strTicker);
+        READWRITE(strName);
+        READWRITE(strChain);
+        READWRITE(strSummary);
+        READWRITE(strDescription);
+        READWRITE(strCreator);
+        READWRITE(strContactURL);
+        READWRITE(strContactEmail);
+        if (!(s.GetType() & SER_GETHASH)) {
+            READWRITE(vchSig);
+        }
+    }
+
+    void ToJson(UniValue& obj) const;
+
+    // FUNCTIONS FOR DEALING WITH DATA STRING
+    void LoadData();
+    void GetData(UniValue& objResult);
+};
+
+#endif

wagerr-from-builder/wagerr

src/tokens/rpctokenwallet.cpp
@@ -12,6 +12,7 @@
 #include "rpc/protocol.h"
 #include "rpc/server.h"
 #include "script/tokengroup.h"
+#include "tokens/tokengroupdocument.h"
 #include "tokens/tokengroupmanager.h"
 #include "utilmoneystr.h"
 #include "utilstrencodings.h"
@@ -1909,6 +1910,73 @@ UniValue listunspenttokens(const JSONRPCRequest& request)
     return results;
 }
 
+UniValue signrawtokendocument(const JSONRPCRequest& request)
+{
+    std::shared_ptr<CWallet> const wallet = GetWalletForJSONRPCRequest(request);
+    CWallet* const pwallet = wallet.get();
+    if (!EnsureWalletIsAvailable(pwallet, request.fHelp)) {
+        return NullUniValue;
+    }
+
+    if (request.fHelp || request.params.size() < 2 || request.params.size() > 3) {
+        throw std::runtime_error(
+                "signrawtokendocument \"data\" \"address\" ( \"verbose\" )\n"
+                "\nSigns the raw token document using the supplied address.\n"
+                "\nArguments:\n"
+                "1. \"data\"           (hex, required) The (unsigned) serialized token document.\n"
+                "2. \"address\"        (string, required) The address used to sign the token document.\n"
+                "3. \"verbose\"        (bool, optional,default=false) Output the json encoded specification instead of the serialized data.\n"
+        );
+    }
+
+    LOCK2(cs_main, pwallet->cs_wallet);
+    RPCTypeCheck(request.params, {UniValue::VSTR, UniValue::VSTR});
+
+    bool fVerbose = false;
+    if (request.params.size() > 2) {
+        std::string sVerbose;
+        std::string p = request.params[2].get_str();
+        std::transform(p.begin(), p.end(), std::back_inserter(sVerbose), ::tolower);
+        fVerbose = (sVerbose == "true");
+    }
+
+    EnsureWalletIsUnlocked(pwallet);
+
+    std::string strHexDescription = request.params[0].get_str();
+    std::string strAddress = request.params[1].get_str();
+    CTxDestination dest = DecodeDestination(strAddress);
+    if (!IsValidDestination(dest)) {
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Wagerr address");
+    }
+    const CKeyID *keyID = boost::get<CKeyID>(&dest);
+    if (!keyID) {
+        throw JSONRPCError(RPC_TYPE_ERROR, "Address does not refer to a key");
+    }
+    CKey vchSecret;
+    if (!pwallet->GetKey(*keyID, vchSecret)) {
+        throw JSONRPCError(RPC_WALLET_ERROR, "Private key for address " + strAddress + " is not known");
+    }
+
+    CDataStream ssTGDocument(ParseHexV(request.params[0], "data"), SER_NETWORK, PROTOCOL_VERSION);
+    CTokenGroupDocument tgDocument;
+    ssTGDocument >> tgDocument;
+
+    if (!tgDocument.Sign(vchSecret)) {
+        throw JSONRPCError(RPC_WALLET_ERROR, "Unable to sign document");
+    }
+
+    if (fVerbose) {
+        UniValue ret(UniValue::VOBJ);
+        tgDocument.ToJson(ret);
+        return ret;
+    }
+    CDataStream ssTGDocumentOut(SER_NETWORK, PROTOCOL_VERSION);
+    ssTGDocumentOut << tgDocument;
+    std::string strData = HexStr(ssTGDocumentOut.begin(), ssTGDocumentOut.end());
+
+    return strData;
+}
+
 static const CRPCCommand commands[] =
 { //  category              name                        actor (function)            argNames
   //  --------------------- --------------------------  --------------------------  ----------
@@ -1919,6 +1987,7 @@ static const CRPCCommand commands[] =
     { "tokens",             "sendtoken",                &sendtoken,                 {} },
     { "tokens",             "configuretoken",           &configuretoken,            {} },
     { "tokens",             "configuremanagementtoken", &configuremanagementtoken,  {} },
+    { "tokens",             "signrawtokendocument",     &signrawtokendocument,      {"data","address","verbose"} },
     { "tokens",             "createtokenauthorities",   &createtokenauthorities,    {} },
     { "tokens",             "listtokenauthorities",     &listtokenauthorities,      {} },
     { "tokens",             "droptokenauthorities",     &droptokenauthorities,      {} },

src/tokens/tokengroupdocument.cpp
@@ -1,0 +1,112 @@
+// Copyright (c) 2019 The ION Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "tokens/tokengroupdocument.h"
+#include "util.h"
+#include <utilstrencodings.h>
+#include <messagesigner.h>
+#include <univalue.h>
+
+/*
+{
+  "atp": "1.0",
+  "data": {
+    "ticker": "MGT",
+    "name": "Management Token",
+    "chain": "WAGERR.testnet",
+    "summary": "The MGT token is a tokenized management key on the WAGERR blockchain with special authorities necessary for: (1) the construction of a token system with coherent economic incentives; (2) the inception of Nucleus Tokens (special tokens that have interrelated monetary policies); and (3) the distribution of rewards that sustain this system of cryptographic tokens on the blockchain.",
+    "description": "The Atomic Token Protocol (ATP) introduces cross-coin and cross-token policy. WAGERR utilizes ATP for its reward system and rights structure. Management Token (MGT), Guardian Validator Token (GVT), and Guardian Validators all participate in an interconnected managent system, and are considered the Nucleus Tokens. The MGT token itself is a tokenized management key with special authorities needed for token inception on the blockchain. The MGT token continues to play a role in the management of and access to special features.",
+    "creator": "The WAGERR Core Developers",
+    "contact": {
+      "url": "https://github.com/wagerr/wagerr"
+    }
+  },
+  "hash": "467723bc0c114a4459e05d8d422ee6766dede23019190a68005290cc053a7e36",
+  "signature": ""
+}
+*/
+void CTokenGroupDocument::ToJson(UniValue& obj) const
+{
+    obj.clear();
+    obj.setObject();
+    obj.pushKV("atp", nVersion);
+    UniValue dataObj(UniValue::VOBJ);
+    dataObj.pushKV("ticker", strTicker);
+    dataObj.pushKV("name", strName);
+    dataObj.pushKV("chain", strChain);
+    dataObj.pushKV("summary", strSummary);
+    dataObj.pushKV("description", strDescription);
+    dataObj.pushKV("creator", strCreator);
+    UniValue dataContactObj(UniValue::VOBJ);
+    dataContactObj.pushKV("url", strContactURL);
+    dataContactObj.pushKV("email", strContactEmail);
+    dataObj.pushKV("contact", dataContactObj);
+    obj.pushKV("data", dataObj);
+    obj.pushKV("hash", GetSignatureHash().GetHex());
+    obj.pushKV("signature", HexStr(vchSig));
+}
+
+
+uint256 CTokenGroupDocument::GetHash() const
+{
+    return SerializeHash(*this);
+}
+
+uint256 CTokenGroupDocument::GetSignatureHash() const
+{
+    return GetHash();
+}
+
+bool CTokenGroupDocument::Sign(const CKey& key)
+{
+    if (!key.IsValid()) {
+        LogPrintf("CTokenGroupDocument::Sign -- signing key is not valid\n");
+        return false;
+    }
+
+    CKeyID pubKeyId = key.GetPubKey().GetID();
+    std::string strError = "";
+
+    uint256 hash = GetSignatureHash();
+    if (!CHashSigner::SignHash(hash, key, vchSig)) {
+        LogPrintf("CTokenGroupDocument::Sign -- SignMessage() failed\n");
+        return false;
+    }
+
+
+    if(!CHashSigner::VerifyHash(hash, pubKeyId, vchSig, strError)) {
+        LogPrintf("CTokenGroupDocument::Sign -- VerifyMessage() failed, error: %s\n", strError);
+        return false;
+    }
+
+    return true;
+}
+
+bool CTokenGroupDocument::CheckSignature(const CKeyID& pubKeyId) const
+{
+    std::string strError = "";
+
+    uint256 hash = GetSignatureHash();
+
+    if(!CHashSigner::VerifyHash(hash, pubKeyId, vchSig, strError)) {
+        LogPrintf("CTokenGroupDocument::Sign -- VerifyMessage() failed, error: %s\n", strError);
+        return false;
+    }
+
+    return true;
+}
+
+bool CTokenGroupDocument::GetSignerKeyID(CKeyID &retKeyidSporkSigner) const
+{
+    CPubKey pubkeyFromSig;
+    uint256 hash = GetSignatureHash();
+    CHashWriter ss(SER_GETHASH, 0);
+    if (!pubkeyFromSig.RecoverCompact(hash, vchSig)) {
+        return false;
+    }
+
+    retKeyidSporkSigner = pubkeyFromSig.GetID();
+    return true;
+}
+

src/tokens/tokengroupdocument.h
@@ -1,0 +1,134 @@
+// Copyright (c) 2019 The ION Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef TOKEN_GROUP_DOCUMENT
+#define TOKEN_GROUP_DOCUMENT
+
+#include <key.h>
+
+class UniValue;
+
+class CTokenGroupDocument
+{
+public:
+    static const uint16_t CURRENT_VERSION = 1;
+
+public:
+    uint16_t nVersion{CURRENT_VERSION};
+
+/*
+{
+  "atp": "1.0",
+  "data": {
+    "ticker": "MGT",
+    "name": "Management Token",
+    "chain": "WAGERR.testnet",
+    "summary": "The MGT token is a tokenized management key on the WAGERR blockchain with special authorities necessary for: (1) the construction of a token system with coherent economic incentives; (2) the inception of Nucleus Tokens (special tokens that have interrelated monetary policies); and (3) the distribution of rewards that sustain this system of cryptographic tokens on the blockchain.",
+    "description": "The Atomic Token Protocol (ATP) introduces cross-coin and cross-token policy. WAGERR utilizes ATP for its reward system and rights structure. Management Token (MGT), Guardian Validator Token (GVT), and Guardian Validators all participate in an interconnected managent system, and are considered the Nucleus Tokens. The MGT token itself is a tokenized management key with special authorities needed for token inception on the blockchain. The MGT token continues to play a role in the management of and access to special features.",
+    "creator": "The WAGERR Core Developers",
+    "contact": {
+      "url": "https://github.com/wagerr/wagerr"
+    }
+  },
+  "hash": "467723bc0c114a4459e05d8d422ee6766dede23019190a68005290cc053a7e36",
+  "signature": ""
+}
+*/
+    // data
+    std::string strTicker;
+    std::string strName;
+    std::string strChain;
+    std::string strSummary;
+    std::string strDescription;
+    std::string strCreator;
+    std::string strContactURL;
+    std::string strContactEmail;
+
+    std::vector<unsigned char> vchSig;
+
+    CTokenGroupDocument() {
+        SetNull();
+    };
+    CTokenGroupDocument(std::string strTicker, std::string strName, std::string strChain, std::string strSummary,
+        std::string strDescription, std::string strCreator, std::string strContactURL, std::string strContactEmail) :
+        strTicker(strTicker), strName(strName), strChain(strChain), strSummary(strSummary), strDescription(strDescription),
+        strCreator(strCreator), strContactURL(strContactURL), strContactEmail(strContactEmail) {};
+
+    void SetNull() {
+        strTicker = "";
+        strName = "";
+        strChain = "";
+        strSummary = "";
+        strDescription = "";
+        strCreator = "";
+        strContactURL = "";
+        strContactEmail = "";
+        vchSig.clear();
+    }
+
+    /**
+     * GetHash returns the double-sha256 hash of the serialized item.
+     */
+    uint256 GetHash() const;
+
+    /**
+     * GetSignatureHash returns the hash of the serialized item
+     * without the signature included. The intent of this method is to get the
+     * hash to be signed.
+     */
+    uint256 GetSignatureHash() const;
+
+    /**
+     * Sign will sign the item with the given key.
+     */
+    bool Sign(const CKey& key);
+
+    /**
+     * CheckSignature will ensure the item signature matches the provided public
+     * key hash.
+     */
+    bool CheckSignature(const CKeyID& pubKeyId) const;
+
+    /**
+     * GetSignerKeyID is used to recover the address of the key used to
+     * sign this item.
+     */
+    bool GetSignerKeyID(CKeyID& retKeyidSporkSigner) const;
+
+    // FUNCTIONS FOR DEALING WITH DATA STRING
+
+    std::string GetDataAsHexString() const;
+    std::string GetDataAsPlainString() const;
+
+    // SERIALIZER
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action)
+    {
+        if (!(s.GetType() & SER_GETHASH)) {
+            READWRITE(nVersion);
+        }
+        READWRITE(strTicker);
+        READWRITE(strName);
+        READWRITE(strChain);
+        READWRITE(strSummary);
+        READWRITE(strDescription);
+        READWRITE(strCreator);
+        READWRITE(strContactURL);
+        READWRITE(strContactEmail);
+        if (!(s.GetType() & SER_GETHASH)) {
+            READWRITE(vchSig);
+        }
+    }
+
+    void ToJson(UniValue& obj) const;
+
+    // FUNCTIONS FOR DEALING WITH DATA STRING
+    void LoadData();
+    void GetData(UniValue& objResult);
+};
+
+#endif