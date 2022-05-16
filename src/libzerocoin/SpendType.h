bytzck/BYTZ

Update Zerocoin

src/libzerocoin/SpendType.h
@@ -1,0 +1,19 @@
+// Copyright (c) 2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef LIBZEROCOIN_SPENDTYPE_H
+#define LIBZEROCOIN_SPENDTYPE_H
+
+#include <cstdint>
+
+namespace libzerocoin {
+    enum SpendType : uint8_t {
+        SPEND, // Used for a typical spend transaction, zBYTZ should be unusable after
+        STAKE, // Used for a spend that occurs as a stake
+        MN_COLLATERAL, // Used when proving ownership of zBYTZ that will be used for masternodes (future)
+        SIGN_MESSAGE // Used to sign messages that do not belong above (future)
+    };
+}
+
+#endif //LIBZEROCOIN_SPENDTYPE_H

src/pos/kernel.cpp
@@ -14,6 +14,8 @@
 #include "timedata.h"
 #include "util.h"
 #include "stakeinput.h"
+#include "zbytz/zbytzchain.h"
+#include "zbytz/accumulators.h"
 
 // v1 modifier interval.
 static const int64_t OLD_MODIFIER_INTERVAL = 2087;
@@ -374,6 +376,32 @@ bool Stake(const CBlockIndex* pindexPrev, CStakeInput* stakeInput, unsigned int 
     return fSuccess;
 }
 
+bool ContextualCheckZerocoinStake(int nPreviousBlockHeight, CStakeInput* stake)
+{
+    if (nPreviousBlockHeight < Params().GetConsensus().nBlockZerocoinV2)
+        return error("%s : zBYTZ stake block is less than allowed start height", __func__);
+
+    if (CZStake* zBYTZ = dynamic_cast<CZStake*>(stake)) {
+        CBlockIndex* pindexFrom = zBYTZ->GetIndexFrom();
+        if (!pindexFrom)
+            return error("%s: failed to get index associated with zBYTZ stake checksum", __func__);
+
+        int depth = (nPreviousBlockHeight + 1) - pindexFrom->nHeight;
+        if (depth < Params().GetConsensus().nZerocoinRequiredStakeDepth)
+            return error("%s : zBYTZ stake does not have required confirmation depth. Current height %d,  stakeInput height %d.", __func__, nPreviousBlockHeight, pindexFrom->nHeight);
+
+        //The checksum needs to be the exact checksum from 200 blocks ago
+        uint256 nCheckpoint200 = chainActive[nPreviousBlockHeight - Params().GetConsensus().nZerocoinRequiredStakeDepth]->GetBlockHeader().nAccumulatorCheckpoint;
+        uint32_t nChecksum200 = ParseChecksum(nCheckpoint200, libzerocoin::AmountToZerocoinDenomination(zBYTZ->GetValue()));
+        if (nChecksum200 != zBYTZ->GetChecksum())
+            return error("%s: accumulator checksum is different than the block 200 blocks previous. stake=%d block200=%d", __func__, zBYTZ->GetChecksum(), nChecksum200);
+    } else {
+        return error("%s: dynamic_cast of stake ptr failed", __func__);
+    }
+
+    return true;
+}
+
 // Check kernel hash target and coinstake signature
 bool initStakeInput(const CBlock block, std::unique_ptr<CStakeInput>& stake, int nPreviousBlockHeight) {
     const CTransaction tx = *block.vtx[1];
@@ -383,7 +411,17 @@ bool initStakeInput(const CBlock block, std::unique_ptr<CStakeInput>& stake, int
     // Kernel (input 0) must match the stake hash target per coin age (nBits)
     const CTxIn& txin = tx.vin[0];
 
-    {
+    //Construct the stakeinput object
+    if (txin.IsZerocoinSpend()) {
+        libzerocoin::CoinSpend spend = TxInToZerocoinSpend(txin);
+        if (spend.getSpendType() != libzerocoin::SpendType::STAKE)
+            return error("%s: spend is using the wrong SpendType (%d)", __func__, (int)spend.getSpendType());
+
+        stake = std::unique_ptr<CStakeInput>(new CZStake(spend));
+
+        if (!ContextualCheckZerocoinStake(nPreviousBlockHeight, stake.get()))
+            return error("%s: staked zBYTZ fails context checks", __func__);
+    } else {
         // First try finding the previous transaction in database
         uint256 hashBlock;
         CTransactionRef txPrev;
@@ -424,6 +462,12 @@ bool CheckProofOfStake(const CBlock block, uint256& hashProofOfStake, std::uniqu
     unsigned int nTxTime = block.nTime;
     const int nBlockFromHeight = pindexfrom->nHeight;
 
+    if (!txin.IsZerocoinSpend() && nPreviousBlockHeight >= Params().GetConsensus().nPublicZCSpends - 1) {
+        //check for maturity (min age/depth) requirements
+        if (!HasStakeMinAgeOrDepth(nPreviousBlockHeight+1, nTxTime, nBlockFromHeight, nBlockFromTime))
+            return error("%s : min age violation - height=%d - nTimeTx=%d, nTimeBlockFrom=%d, nHeightBlockFrom=%d",
+                             __func__, nPreviousBlockHeight, nTxTime, nBlockFromTime, nBlockFromHeight);
+    }
     if (!CheckStakeKernelHash(pindex->pprev, block.nBits, stake.get(), nTxTime, hashProofOfStake, true)) {
         return error("%s : INFO: check kernel failed on coinstake %s, hashProof=%s", __func__,
                      tx->GetHash().GetHex(), hashProofOfStake.GetHex());

src/pos/stakeinput.cpp
@@ -9,8 +9,188 @@
 #include "stakeinput.h"
 #include "validation.h"
 #include "wallet/wallet.h"
+#include "zbytz/accumulators.h"
+#include "zbytz/deterministicmint.h"
 
 typedef std::vector<unsigned char> valtype;
+
+CZStake::CZStake(const libzerocoin::CoinSpend& spend)
+{
+    this->nChecksum = spend.getAccumulatorChecksum();
+    this->denom = spend.getDenomination();
+    uint256 nSerial = ArithToUint256(spend.getCoinSerialNumber().getuint256());
+    this->hashSerial = Hash(nSerial.begin(), nSerial.end());
+    fMint = false;
+}
+
+int CZStake::GetChecksumHeightFromMint()
+{
+    int nHeightChecksum = chainActive.Height() - Params().GetConsensus().nZerocoinRequiredStakeDepth;
+
+    //Need to return the first occurance of this checksum in order for the validation process to identify a specific
+    //block height
+    uint32_t nChecksum = 0;
+    nChecksum = ParseChecksum(chainActive[nHeightChecksum]->GetBlockHeader().nAccumulatorCheckpoint, denom);
+    return GetChecksumHeight(nChecksum, denom);
+}
+
+int CZStake::GetChecksumHeightFromSpend()
+{
+    return GetChecksumHeight(nChecksum, denom);
+}
+
+uint32_t CZStake::GetChecksum()
+{
+    return nChecksum;
+}
+
+// The zBYTZ block index is the first appearance of the accumulator checksum that was used in the spend
+// note that this also means when staking that this checksum should be from a block that is beyond 60 minutes old and
+// 100 blocks deep.
+CBlockIndex* CZStake::GetIndexFrom()
+{
+    if (pindexFrom)
+        return pindexFrom;
+
+    int nHeightChecksum = 0;
+
+    if (fMint)
+        nHeightChecksum = GetChecksumHeightFromMint();
+    else
+        nHeightChecksum = GetChecksumHeightFromSpend();
+
+    if (nHeightChecksum < Params().GetConsensus().nZerocoinStartHeight || nHeightChecksum > chainActive.Height()) {
+        pindexFrom = nullptr;
+    } else {
+        //note that this will be a nullptr if the height DNE
+        pindexFrom = chainActive[nHeightChecksum];
+    }
+
+    return pindexFrom;
+}
+
+CAmount CZStake::GetValue()
+{
+    return denom * COIN;
+}
+
+//Use the first accumulator checkpoint that occurs 60 minutes after the block being staked from
+// In case of regtest, next accumulator of 60 blocks after the block being staked from
+bool CZStake::GetModifier(uint64_t& nStakeModifier)
+{
+    CBlockIndex* pindex = GetIndexFrom();
+    if (!pindex)
+        return error("%s: failed to get index from", __func__);
+
+    if(Params().NetworkIDString() == CBaseChainParams::REGTEST) {
+        // Stake modifier is fixed for now, move it to 60 blocks after this pindex in the future..
+        nStakeModifier = pindexFrom->nStakeModifier;
+        return true;
+    }
+
+    int64_t nTimeBlockFrom = pindex->GetBlockTime();
+    while (true) {
+        if (pindex->GetBlockTime() - nTimeBlockFrom > 60 * 60) {
+            nStakeModifier = UintToArith256(pindex->GetBlockHeader().nAccumulatorCheckpoint).GetLow64();
+            return true;
+        }
+
+        if (pindex->nHeight + 1 <= chainActive.Height())
+            pindex = chainActive.Next(pindex);
+        else
+            return false;
+    }
+}
+
+CDataStream CZStake::GetUniqueness()
+{
+    //The unique identifier for a zBYTZ is a hash of the serial
+    CDataStream ss(SER_GETHASH, 0);
+    ss << hashSerial;
+    return ss;
+}
+
+bool CZStake::CreateTxIn(CWallet* pwallet, CTxIn& txIn, uint256 hashTxOut)
+{
+    return false;
+/*
+    CBlockIndex* pindexCheckpoint = GetIndexFrom();
+    if (!pindexCheckpoint)
+        return error("%s: failed to find checkpoint block index", __func__);
+
+    CZerocoinMint mint;
+    if (!pwallet->GetMintFromStakeHash(hashSerial, mint))
+        return error("%s: failed to fetch mint associated with serial hash %s", __func__, hashSerial.GetHex());
+
+    if (libzerocoin::ExtractVersionFromSerial(mint.GetSerialNumber()) < 2)
+        return error("%s: serial extract is less than v2", __func__);
+
+    CZerocoinSpendReceipt receipt;
+    if (!pwallet->MintToTxIn(mint, hashTxOut, txIn, receipt, libzerocoin::SpendType::STAKE, pindexCheckpoint))
+        return error("%s", receipt.GetStatusMessage());
+
+    return true;
+*/
+}
+
+bool CZStake::CreateTxOuts(CWallet* pwallet, std::vector<CTxOut>& vout, CAmount nTotal)
+{
+    return false;
+/*
+    //Create an output returning the zBYTZ that was staked
+    CTxOut outReward;
+    libzerocoin::CoinDenomination denomStaked = libzerocoin::AmountToZerocoinDenomination(this->GetValue());
+    CDeterministicMint dMint;
+    if (!pwallet->CreateZBYTZOutPut(denomStaked, outReward, dMint))
+        return error("%s: failed to create zBYTZ output", __func__);
+    vout.emplace_back(outReward);
+
+    //Add new staked denom to our wallet
+    if (!pwallet->DatabaseMint(dMint))
+        return error("%s: failed to database the staked zBYTZ", __func__);
+
+    CAmount toMint = (nTotal - this->GetValue()) / 2;
+    CAmount nRemaining = 0;
+    while (toMint >= 1 * COIN) {
+        libzerocoin::CoinDenomination denomination = libzerocoin::AmountToClosestDenomination(toMint, nRemaining);
+
+        if (denomination == libzerocoin::ZQ_ERROR)
+            return error("%s: failed to create zBYTZ output", __func__);
+
+        CAmount nValueNewMint = libzerocoin::ZerocoinDenominationToAmount(denomination);
+        toMint -= nValueNewMint;
+
+        CTxOut out;
+        CDeterministicMint dMintReward;
+        if (!pwallet->CreateZBYTZOutPut(denomination, out, dMintReward))
+            return error("%s: failed to create zBYTZ output", __func__);
+        vout.emplace_back(out);
+
+        if (!pwallet->DatabaseMint(dMintReward))
+            return error("%s: failed to database mint reward", __func__);
+    }
+
+    return true;
+*/
+}
+
+bool CZStake::GetTxFrom(CTransactionRef& tx)
+{
+    return false;
+}
+
+/*
+bool CZStake::MarkSpent(CWallet *pwallet, const uint256& txid)
+{
+    CxBYTZTracker* zbytzTracker = pwallet->zbytzTracker.get();
+    CMintMeta meta;
+    if (!zbytzTracker->GetMetaFromStakeHash(hashSerial, meta))
+        return error("%s: tracker does not have serialhash", __func__);
+
+    zbytzTracker->SetPubcoinUsed(meta.hashPubcoin, txid);
+    return true;
+}
+*/
 
 //!Bytz Stake
 bool CStake::SetInput(CTransactionRef txPrev, unsigned int n)

src/zbytz/zerocoindb.h
@@ -1,0 +1,41 @@
+// Copyright (c) 2017-2019 The PIVX developers
+// Copyright (c) 2019 The ION Core Developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef PIVX_ZEROCOINDB_H
+#define PIVX_ZEROCOINDB_H
+
+#include "dbwrapper.h"
+#include "zbytz/zerocoin.h"
+#include "libzerocoin/Coin.h"
+#include "libzerocoin/CoinSpend.h"
+
+/** Zerocoin database (zerocoin/) */
+class CZerocoinDB : public CDBWrapper
+{
+public:
+    CZerocoinDB(size_t nCacheSize, bool fMemory = false, bool fWipe = false);
+
+private:
+    CZerocoinDB(const CZerocoinDB&);
+    void operator=(const CZerocoinDB&);
+
+public:
+    /** Write zBYTZ mints to the zerocoinDB in a batch */
+    bool WriteCoinMintBatch(const std::vector<std::pair<libzerocoin::PublicCoin, uint256> >& mintInfo);
+    bool ReadCoinMint(const CBigNum& bnPubcoin, uint256& txHash);
+    bool ReadCoinMint(const uint256& hashPubcoin, uint256& hashTx);
+    /** Write zBYTZ spends to the zerocoinDB in a batch */
+    bool WriteCoinSpendBatch(const std::vector<std::pair<libzerocoin::CoinSpend, uint256> >& spendInfo);
+    bool ReadCoinSpend(const CBigNum& bnSerial, uint256& txHash);
+    bool ReadCoinSpend(const uint256& hashSerial, uint256 &txHash);
+    bool EraseCoinMint(const CBigNum& bnPubcoin);
+    bool EraseCoinSpend(const CBigNum& bnSerial);
+    bool WipeCoins(std::string strType);
+    bool WriteAccumulatorValue(const uint32_t& nChecksum, const CBigNum& bnValue);
+    bool ReadAccumulatorValue(const uint32_t& nChecksum, CBigNum& bnValue);
+    bool EraseAccumulatorValue(const uint32_t& nChecksum);
+};
+
+#endif //PIVX_ZEROCOINDB_H

wagerr-from-builder/wagerr

src/libzerocoin/SpendType.h
@@ -1,0 +1,19 @@
+// Copyright (c) 2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef LIBZEROCOIN_SPENDTYPE_H
+#define LIBZEROCOIN_SPENDTYPE_H
+
+#include <cstdint>
+
+namespace libzerocoin {
+    enum SpendType : uint8_t {
+        SPEND, // Used for a typical spend transaction, zWAGERR should be unusable after
+        STAKE, // Used for a spend that occurs as a stake
+        MN_COLLATERAL, // Used when proving ownership of zWAGERR that will be used for masternodes (future)
+        SIGN_MESSAGE // Used to sign messages that do not belong above (future)
+    };
+}
+
+#endif //LIBZEROCOIN_SPENDTYPE_H

src/pos/kernel.cpp
@@ -14,6 +14,8 @@
 #include "timedata.h"
 #include "util.h"
 #include "stakeinput.h"
+#include "zwgr/zwgrchain.h"
+#include "zwgr/accumulators.h"
 
 // v1 modifier interval.
 static const int64_t OLD_MODIFIER_INTERVAL = 2087;
@@ -374,6 +376,32 @@ bool Stake(const CBlockIndex* pindexPrev, CStakeInput* stakeInput, unsigned int 
     return fSuccess;
 }
 
+bool ContextualCheckZerocoinStake(int nPreviousBlockHeight, CStakeInput* stake)
+{
+    if (nPreviousBlockHeight < Params().GetConsensus().nBlockZerocoinV2)
+        return error("%s : zWAGERR stake block is less than allowed start height", __func__);
+
+    if (CZStake* zWAGERR = dynamic_cast<CZStake*>(stake)) {
+        CBlockIndex* pindexFrom = zWAGERR->GetIndexFrom();
+        if (!pindexFrom)
+            return error("%s: failed to get index associated with zWAGERR stake checksum", __func__);
+
+        int depth = (nPreviousBlockHeight + 1) - pindexFrom->nHeight;
+        if (depth < Params().GetConsensus().nZerocoinRequiredStakeDepth)
+            return error("%s : zWAGERR stake does not have required confirmation depth. Current height %d,  stakeInput height %d.", __func__, nPreviousBlockHeight, pindexFrom->nHeight);
+
+        //The checksum needs to be the exact checksum from 200 blocks ago
+        uint256 nCheckpoint200 = chainActive[nPreviousBlockHeight - Params().GetConsensus().nZerocoinRequiredStakeDepth]->GetBlockHeader().nAccumulatorCheckpoint;
+        uint32_t nChecksum200 = ParseChecksum(nCheckpoint200, libzerocoin::AmountToZerocoinDenomination(zWAGERR->GetValue()));
+        if (nChecksum200 != zWAGERR->GetChecksum())
+            return error("%s: accumulator checksum is different than the block 200 blocks previous. stake=%d block200=%d", __func__, zWAGERR->GetChecksum(), nChecksum200);
+    } else {
+        return error("%s: dynamic_cast of stake ptr failed", __func__);
+    }
+
+    return true;
+}
+
 // Check kernel hash target and coinstake signature
 bool initStakeInput(const CBlock block, std::unique_ptr<CStakeInput>& stake, int nPreviousBlockHeight) {
     const CTransaction tx = *block.vtx[1];
@@ -383,7 +411,17 @@ bool initStakeInput(const CBlock block, std::unique_ptr<CStakeInput>& stake, int
     // Kernel (input 0) must match the stake hash target per coin age (nBits)
     const CTxIn& txin = tx.vin[0];
 
-    {
+    //Construct the stakeinput object
+    if (txin.IsZerocoinSpend()) {
+        libzerocoin::CoinSpend spend = TxInToZerocoinSpend(txin);
+        if (spend.getSpendType() != libzerocoin::SpendType::STAKE)
+            return error("%s: spend is using the wrong SpendType (%d)", __func__, (int)spend.getSpendType());
+
+        stake = std::unique_ptr<CStakeInput>(new CZStake(spend));
+
+        if (!ContextualCheckZerocoinStake(nPreviousBlockHeight, stake.get()))
+            return error("%s: staked zWAGERR fails context checks", __func__);
+    } else {
         // First try finding the previous transaction in database
         uint256 hashBlock;
         CTransactionRef txPrev;
@@ -424,6 +462,12 @@ bool CheckProofOfStake(const CBlock block, uint256& hashProofOfStake, std::uniqu
     unsigned int nTxTime = block.nTime;
     const int nBlockFromHeight = pindexfrom->nHeight;
 
+    if (!txin.IsZerocoinSpend() && nPreviousBlockHeight >= Params().GetConsensus().nPublicZCSpends - 1) {
+        //check for maturity (min age/depth) requirements
+        if (!HasStakeMinAgeOrDepth(nPreviousBlockHeight+1, nTxTime, nBlockFromHeight, nBlockFromTime))
+            return error("%s : min age violation - height=%d - nTimeTx=%d, nTimeBlockFrom=%d, nHeightBlockFrom=%d",
+                             __func__, nPreviousBlockHeight, nTxTime, nBlockFromTime, nBlockFromHeight);
+    }
     if (!CheckStakeKernelHash(pindex->pprev, block.nBits, stake.get(), nTxTime, hashProofOfStake, true)) {
         return error("%s : INFO: check kernel failed on coinstake %s, hashProof=%s", __func__,
                      tx->GetHash().GetHex(), hashProofOfStake.GetHex());

src/pos/stakeinput.cpp
@@ -9,8 +9,188 @@
 #include "stakeinput.h"
 #include "validation.h"
 #include "wallet/wallet.h"
+#include "zwgr/accumulators.h"
+#include "zwgr/deterministicmint.h"
 
 typedef std::vector<unsigned char> valtype;
+
+CZStake::CZStake(const libzerocoin::CoinSpend& spend)
+{
+    this->nChecksum = spend.getAccumulatorChecksum();
+    this->denom = spend.getDenomination();
+    uint256 nSerial = ArithToUint256(spend.getCoinSerialNumber().getuint256());
+    this->hashSerial = Hash(nSerial.begin(), nSerial.end());
+    fMint = false;
+}
+
+int CZStake::GetChecksumHeightFromMint()
+{
+    int nHeightChecksum = chainActive.Height() - Params().GetConsensus().nZerocoinRequiredStakeDepth;
+
+    //Need to return the first occurance of this checksum in order for the validation process to identify a specific
+    //block height
+    uint32_t nChecksum = 0;
+    nChecksum = ParseChecksum(chainActive[nHeightChecksum]->GetBlockHeader().nAccumulatorCheckpoint, denom);
+    return GetChecksumHeight(nChecksum, denom);
+}
+
+int CZStake::GetChecksumHeightFromSpend()
+{
+    return GetChecksumHeight(nChecksum, denom);
+}
+
+uint32_t CZStake::GetChecksum()
+{
+    return nChecksum;
+}
+
+// The zWAGERR block index is the first appearance of the accumulator checksum that was used in the spend
+// note that this also means when staking that this checksum should be from a block that is beyond 60 minutes old and
+// 100 blocks deep.
+CBlockIndex* CZStake::GetIndexFrom()
+{
+    if (pindexFrom)
+        return pindexFrom;
+
+    int nHeightChecksum = 0;
+
+    if (fMint)
+        nHeightChecksum = GetChecksumHeightFromMint();
+    else
+        nHeightChecksum = GetChecksumHeightFromSpend();
+
+    if (nHeightChecksum < Params().GetConsensus().nZerocoinStartHeight || nHeightChecksum > chainActive.Height()) {
+        pindexFrom = nullptr;
+    } else {
+        //note that this will be a nullptr if the height DNE
+        pindexFrom = chainActive[nHeightChecksum];
+    }
+
+    return pindexFrom;
+}
+
+CAmount CZStake::GetValue()
+{
+    return denom * COIN;
+}
+
+//Use the first accumulator checkpoint that occurs 60 minutes after the block being staked from
+// In case of regtest, next accumulator of 60 blocks after the block being staked from
+bool CZStake::GetModifier(uint64_t& nStakeModifier)
+{
+    CBlockIndex* pindex = GetIndexFrom();
+    if (!pindex)
+        return error("%s: failed to get index from", __func__);
+
+    if(Params().NetworkIDString() == CBaseChainParams::REGTEST) {
+        // Stake modifier is fixed for now, move it to 60 blocks after this pindex in the future..
+        nStakeModifier = pindexFrom->nStakeModifier;
+        return true;
+    }
+
+    int64_t nTimeBlockFrom = pindex->GetBlockTime();
+    while (true) {
+        if (pindex->GetBlockTime() - nTimeBlockFrom > 60 * 60) {
+            nStakeModifier = UintToArith256(pindex->GetBlockHeader().nAccumulatorCheckpoint).GetLow64();
+            return true;
+        }
+
+        if (pindex->nHeight + 1 <= chainActive.Height())
+            pindex = chainActive.Next(pindex);
+        else
+            return false;
+    }
+}
+
+CDataStream CZStake::GetUniqueness()
+{
+    //The unique identifier for a zWAGERR is a hash of the serial
+    CDataStream ss(SER_GETHASH, 0);
+    ss << hashSerial;
+    return ss;
+}
+
+bool CZStake::CreateTxIn(CWallet* pwallet, CTxIn& txIn, uint256 hashTxOut)
+{
+    return false;
+/*
+    CBlockIndex* pindexCheckpoint = GetIndexFrom();
+    if (!pindexCheckpoint)
+        return error("%s: failed to find checkpoint block index", __func__);
+
+    CZerocoinMint mint;
+    if (!pwallet->GetMintFromStakeHash(hashSerial, mint))
+        return error("%s: failed to fetch mint associated with serial hash %s", __func__, hashSerial.GetHex());
+
+    if (libzerocoin::ExtractVersionFromSerial(mint.GetSerialNumber()) < 2)
+        return error("%s: serial extract is less than v2", __func__);
+
+    CZerocoinSpendReceipt receipt;
+    if (!pwallet->MintToTxIn(mint, hashTxOut, txIn, receipt, libzerocoin::SpendType::STAKE, pindexCheckpoint))
+        return error("%s", receipt.GetStatusMessage());
+
+    return true;
+*/
+}
+
+bool CZStake::CreateTxOuts(CWallet* pwallet, std::vector<CTxOut>& vout, CAmount nTotal)
+{
+    return false;
+/*
+    //Create an output returning the zWAGERR that was staked
+    CTxOut outReward;
+    libzerocoin::CoinDenomination denomStaked = libzerocoin::AmountToZerocoinDenomination(this->GetValue());
+    CDeterministicMint dMint;
+    if (!pwallet->CreateZWGROutPut(denomStaked, outReward, dMint))
+        return error("%s: failed to create zWAGERR output", __func__);
+    vout.emplace_back(outReward);
+
+    //Add new staked denom to our wallet
+    if (!pwallet->DatabaseMint(dMint))
+        return error("%s: failed to database the staked zWAGERR", __func__);
+
+    CAmount toMint = (nTotal - this->GetValue()) / 2;
+    CAmount nRemaining = 0;
+    while (toMint >= 1 * COIN) {
+        libzerocoin::CoinDenomination denomination = libzerocoin::AmountToClosestDenomination(toMint, nRemaining);
+
+        if (denomination == libzerocoin::ZQ_ERROR)
+            return error("%s: failed to create zWAGERR output", __func__);
+
+        CAmount nValueNewMint = libzerocoin::ZerocoinDenominationToAmount(denomination);
+        toMint -= nValueNewMint;
+
+        CTxOut out;
+        CDeterministicMint dMintReward;
+        if (!pwallet->CreateZWGROutPut(denomination, out, dMintReward))
+            return error("%s: failed to create zWAGERR output", __func__);
+        vout.emplace_back(out);
+
+        if (!pwallet->DatabaseMint(dMintReward))
+            return error("%s: failed to database mint reward", __func__);
+    }
+
+    return true;
+*/
+}
+
+bool CZStake::GetTxFrom(CTransactionRef& tx)
+{
+    return false;
+}
+
+/*
+bool CZStake::MarkSpent(CWallet *pwallet, const uint256& txid)
+{
+    CxWAGERRTracker* zwgrTracker = pwallet->zwgrTracker.get();
+    CMintMeta meta;
+    if (!zwgrTracker->GetMetaFromStakeHash(hashSerial, meta))
+        return error("%s: tracker does not have serialhash", __func__);
+
+    zwgrTracker->SetPubcoinUsed(meta.hashPubcoin, txid);
+    return true;
+}
+*/
 
 //!Wagerr Stake
 bool CStake::SetInput(CTransactionRef txPrev, unsigned int n)

src/zwgr/zerocoindb.h
@@ -1,0 +1,41 @@
+// Copyright (c) 2017-2019 The PIVX developers
+// Copyright (c) 2019 The ION Core Developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef PIVX_ZEROCOINDB_H
+#define PIVX_ZEROCOINDB_H
+
+#include "dbwrapper.h"
+#include "zwgr/zerocoin.h"
+#include "libzerocoin/Coin.h"
+#include "libzerocoin/CoinSpend.h"
+
+/** Zerocoin database (zerocoin/) */
+class CZerocoinDB : public CDBWrapper
+{
+public:
+    CZerocoinDB(size_t nCacheSize, bool fMemory = false, bool fWipe = false);
+
+private:
+    CZerocoinDB(const CZerocoinDB&);
+    void operator=(const CZerocoinDB&);
+
+public:
+    /** Write zWAGERR mints to the zerocoinDB in a batch */
+    bool WriteCoinMintBatch(const std::vector<std::pair<libzerocoin::PublicCoin, uint256> >& mintInfo);
+    bool ReadCoinMint(const CBigNum& bnPubcoin, uint256& txHash);
+    bool ReadCoinMint(const uint256& hashPubcoin, uint256& hashTx);
+    /** Write zWAGERR spends to the zerocoinDB in a batch */
+    bool WriteCoinSpendBatch(const std::vector<std::pair<libzerocoin::CoinSpend, uint256> >& spendInfo);
+    bool ReadCoinSpend(const CBigNum& bnSerial, uint256& txHash);
+    bool ReadCoinSpend(const uint256& hashSerial, uint256 &txHash);
+    bool EraseCoinMint(const CBigNum& bnPubcoin);
+    bool EraseCoinSpend(const CBigNum& bnSerial);
+    bool WipeCoins(std::string strType);
+    bool WriteAccumulatorValue(const uint32_t& nChecksum, const CBigNum& bnValue);
+    bool ReadAccumulatorValue(const uint32_t& nChecksum, CBigNum& bnValue);
+    bool EraseAccumulatorValue(const uint32_t& nChecksum);
+};
+
+#endif //PIVX_ZEROCOINDB_H