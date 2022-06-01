c206655f54e6b8811df35916a37e7dbd0528ff94
wagerrck <104495875+wagerrck@users.noreply.github.com>
2022-06-01 15:33:14 +0100

    Revert spork.cpp and block.cpp

--- src/spork.cpp
+++ src/spork.cpp
@@ -356,7 +356,7 @@ std::string CSporkManager::ToString() const
 
 uint256 CSporkMessage::GetHash() const
 {
-    return SerializeHash(*this);
+    return HashQuark(BEGIN(nSporkID), END(nTimeSigned));
 }
 
 uint256 CSporkMessage::GetSignatureHash() const
