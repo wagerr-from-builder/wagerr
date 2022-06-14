bytzck/BYTZ

Update token help output
- Update gettokenbalance to show that an address parameter is accepted
- Update sendtoken to show that multiple recipients and amounts can be specified

src/tokens/rpctokenwallet.cpp
@@ -340,11 +340,12 @@ extern UniValue gettokenbalance(const JSONRPCRequest& request)
 
     if (request.fHelp)
         throw std::runtime_error(
-            "gettokenbalance ( \"groupid\" )\n"
+            "gettokenbalance ( \"groupid\" ) ( \"address\" )\n"
             "\nIf groupID is not specified, returns all tokens with a balance (including token authorities).\n"
             "If a groupID is specified, returns the balance of the specified token group.\n"
             "\nArguments:\n"
-            "1. \"groupid\" (string, optional) the token group identifier\n"
+            "1. \"groupid\" (string, optional) the token group identifier to filter\n"
+            "2. \"address\" (string, optional) the Bytz address to filter\n"
             "\n"
             "\nExamples:\n" +
             HelpExampleCli("gettokenbalance", "groupid bytzreg1zwm0kzlyptdmwy3849fd6z5epesnjkruqlwlv02u7y6ymf75nk4qs6u85re") +
@@ -722,8 +723,8 @@ extern UniValue sendtoken(const JSONRPCRequest& request)
 
     if (request.fHelp || request.params.size() < 1)
         throw std::runtime_error(
-            "sendtoken \"groupid\" \"address\" amount \n"
-            "\nSends token to a given address.\n"
+            "sendtoken \"groupid\" \"address\" amount ( \"address\" amount ) ( .. ) \n"
+            "\nSends token to a given address. Specify multiple addresses and amounts for multiple recipients.\n"
             "\n"
             "1. \"groupid\"     (string, required) the group identifier\n"
             "2. \"address\"     (string, required) the destination address\n"

wagerr-from-builder/wagerr

src/tokens/rpctokenwallet.cpp
@@ -340,11 +340,12 @@ extern UniValue gettokenbalance(const JSONRPCRequest& request)
 
     if (request.fHelp)
         throw std::runtime_error(
-            "gettokenbalance ( \"groupid\" )\n"
+            "gettokenbalance ( \"groupid\" ) ( \"address\" )\n"
             "\nIf groupID is not specified, returns all tokens with a balance (including token authorities).\n"
             "If a groupID is specified, returns the balance of the specified token group.\n"
             "\nArguments:\n"
-            "1. \"groupid\" (string, optional) the token group identifier\n"
+            "1. \"groupid\" (string, optional) the token group identifier to filter\n"
+            "2. \"address\" (string, optional) the Wagerr address to filter\n"
             "\n"
             "\nExamples:\n" +
             HelpExampleCli("gettokenbalance", "groupid wagerrreg1zwm0kzlyptdmwy3849fd6z5epesnjkruqlwlv02u7y6ymf75nk4qs6u85re") +
@@ -722,8 +723,8 @@ extern UniValue sendtoken(const JSONRPCRequest& request)
 
     if (request.fHelp || request.params.size() < 1)
         throw std::runtime_error(
-            "sendtoken \"groupid\" \"address\" amount \n"
-            "\nSends token to a given address.\n"
+            "sendtoken \"groupid\" \"address\" amount ( \"address\" amount ) ( .. ) \n"
+            "\nSends token to a given address. Specify multiple addresses and amounts for multiple recipients.\n"
             "\n"
             "1. \"groupid\"     (string, required) the group identifier\n"
             "2. \"address\"     (string, required) the destination address\n"