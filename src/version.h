5d55200e7fcd15b29856c26318b07ce56457f896
wagerrck <104495875+wagerrck@users.noreply.github.com>
2022-06-16 10:17:39 +0100

    Change version numbers

--- src/version.h
+++ src/version.h
@@ -17,42 +17,41 @@ static const int PROTOCOL_VERSION = 70929;
 static const int INIT_PROTO_VERSION = 214;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 70238;
+static const int GETHEADERS_VERSION = 70929;
 
 //! New spork protocol
-static const int MIN_SPORK_VERSION = 70926;
+static const int MIN_SPORK_VERSION = 70929;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70926;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70928;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70929;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70929;
 
 //! minimum proto version of masternode to accept in DKGs
-static const int MIN_MASTERNODE_PROTO_VERSION = 70926;
+static const int MIN_MASTERNODE_PROTO_VERSION = 70929;
 
 //! minimum proto version for governance sync and messages
-static const int MIN_GOVERNANCE_PEER_PROTO_VERSION = 70924;
+static const int MIN_GOVERNANCE_PEER_PROTO_VERSION = 70929;
 
 //! minimum proto version to broadcast governance messages from banned masternodes
-static const int GOVERNANCE_POSE_BANNED_VOTES_VERSION = 70926;
+static const int GOVERNANCE_POSE_BANNED_VOTES_VERSION = 70929;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 70926;
+static const int CADDR_TIME_VERSION = 70912;
 
 //! introduction of LLMQs
-static const int LLMQS_PROTO_VERSION = 70925;
+static const int LLMQS_PROTO_VERSION = 70929;
 
 //! introduction of SENDDSQUEUE
 //! TODO we can remove this in 0.15.0.0
-static const int SENDDSQUEUE_PROTO_VERSION = 70925;
+static const int SENDDSQUEUE_PROTO_VERSION = 70929;
 
 //! minimum peer version accepted by mixing pool
-static const int MIN_COINJOIN_PEER_PROTO_VERSION = 70924;
-
+static const int MIN_COINJOIN_PEER_PROTO_VERSION = 70929;
 //! protocol version is included in MNAUTH starting with this version
-static const int MNAUTH_NODE_VER_VERSION = 70926;
+static const int MNAUTH_NODE_VER_VERSION = 70929;
 
 //! introduction of QGETDATA/QDATA messages
-static const int LLMQ_DATA_MESSAGES_VERSION = 70926;
+static const int LLMQ_DATA_MESSAGES_VERSION = 70929;
 
 #endif // BITCOIN_VERSION_H
