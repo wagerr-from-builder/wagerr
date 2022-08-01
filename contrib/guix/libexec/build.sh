92bcdd26d611554e9fc5aa8d5941bc6e225b051b
wagerrck <104495875+wagerrck@users.noreply.github.com>
2022-08-01 12:53:45 +0100

    Change of branding again

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -213,8 +213,8 @@ make -C depends --jobs="$JOBS" HOST="$HOST" \
                                    x86_64_linux_RANLIB=x86_64-linux-gnu-ranlib \
                                    x86_64_linux_NM=x86_64-linux-gnu-nm \
                                    x86_64_linux_STRIP=x86_64-linux-gnu-strip \
-                                   qt_config_opts_i686_linux='-platform linux-g++ -xplatform bitcoin-linux-g++' \
-                                   qt_config_opts_x86_64_linux='-platform linux-g++ -xplatform bitcoin-linux-g++' \
+                                   qt_config_opts_i686_linux='-platform linux-g++ -xplatform wagerr-linux-g++' \
+                                   qt_config_opts_x86_64_linux='-platform linux-g++ -xplatform wagerr-linux-g++' \
                                    FORCE_USE_SYSTEM_CLANG=1
 
 
@@ -299,7 +299,7 @@ mkdir -p "$DISTSRC"
 
     sed -i.old 's/-lstdc++ //g' config.status libtool src/univalue/config.status src/univalue/libtool
 
-    # Build Bitcoin Core
+    # Build Wagerr Core
     make --jobs="$JOBS" ${V:+V=1}
 
     # Check that symbol/security checks tools are sane.
@@ -318,12 +318,12 @@ mkdir -p "$DISTSRC"
             ;;
     esac
 
-    # Setup the directory where our Bitcoin Core build for HOST will be
+    # Setup the directory where our Wagerr Core build for HOST will be
     # installed. This directory will also later serve as the input for our
     # binary tarballs.
     INSTALLPATH="${PWD}/installed/${DISTNAME}"
     mkdir -p "${INSTALLPATH}"
-    # Install built Bitcoin Core to $INSTALLPATH
+    # Install built Wagerr Core to $INSTALLPATH
     case "$HOST" in
         *darwin*)
             make install-strip DESTDIR="${INSTALLPATH}" ${V:+V=1}
