33ffb4a5dfd9348fac99627425ba74f6459d5850
wagerrck <104495875+wagerrck@users.noreply.github.com>
2022-08-11 13:09:25 +0100

    Change packages

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,26 +1,29 @@
-packages:=boost openssl libevent zeromq gmp backtrace cmake bls-dash
+packages:=boost libevent gmp
 
-qt_native_packages = native_protobuf
-qt_packages = qrencode protobuf zlib
+sse2_packages = openssl
 
-qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon
+qt_packages = zlib
+
+qrencode_packages = qrencode
+
+qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig
+qt_android_packages=qt
 
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
 
-wallet_packages=bdb
+bdb_packages=bdb
+sqlite_packages=sqlite
 
 zmq_packages=zeromq
 
 upnp_packages=miniupnpc
 
-darwin_native_packages = native_ds_store native_mac_alias
+multiprocess_packages = libmultiprocess capnp
+multiprocess_native_packages = native_libmultiprocess native_capnp
+
+darwin_native_packages = native_biplist native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_libtapi native_libdmg-hfsplus
-
-ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
-darwin_native_packages+= native_clang
-endif
-
+darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
 endif
