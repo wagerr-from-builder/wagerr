9a8658f0ce52916abe64e6203a071e00ed420e31
wagerrck <104495875+wagerrck@users.noreply.github.com>
2022-08-15 12:43:30 +0100

    Depends change 8

--- depends/packages/native_libdmg-hfsplus.mk
+++ depends/packages/native_libdmg-hfsplus.mk
@@ -12,7 +12,7 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_config_cmds
-  $(host_prefix)/bin/cmake -DCMAKE_INSTALL_PREFIX:PATH=$(build_prefix)/bin ..
+  cmake -DCMAKE_INSTALL_PREFIX:PATH=$(build_prefix)/bin ..
 endef
 
 define $(package)_build_cmds
