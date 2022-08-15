9e3f83d88cb62d3a7b72dbe928c729082ce80fbe
wagerrck <104495875+wagerrck@users.noreply.github.com>
2022-08-15 11:34:19 +0100

    Try guix on clean build 5

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -4,6 +4,8 @@ OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
 LD64_VERSION=253.9
 clang_prog=$(shell $(SHELL) $(.SHELLFLAGS) "command -v clang")
 clangxx_prog=$(shell $(SHELL) $(.SHELLFLAGS) "command -v clang++")
+
+clang_resource_dir=$(shell clang -print-resource-dir)
 darwin_CC=$(clang_prog) -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION)
 darwin_CXX=$(clangxx_prog) -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++
 
@@ -16,4 +18,6 @@ darwin_release_CXXFLAGS=$(darwin_release_CFLAGS)
 darwin_debug_CFLAGS=-O1
 darwin_debug_CXXFLAGS=$(darwin_debug_CFLAGS)
 
-darwin_native_toolchain=native_cctools
+#darwin_native_toolchain=native_cctools
+darwin_native_toolchain=
+darwin_cmake_system=Darwin
