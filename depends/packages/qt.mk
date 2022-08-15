d5f3358727f099062b899047dc532650256fd7b9
wagerrck <104495875+wagerrck@users.noreply.github.com>
2022-08-15 13:31:48 +0100

    Add cmake to darwin packages

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -229,6 +229,7 @@ define $(package)_config_cmds
   export PKG_CONFIG_LIBDIR=$(host_prefix)/lib/pkgconfig && \
   export PKG_CONFIG_PATH=$(host_prefix)/share/pkgconfig && \
   export OPENSSL_LIBS=$(host_prefix)/lib && \
+  CONFIG+=sdk_no_version_check && \
   cd qtbase && \
   ./configure -top-level $($(package)_config_opts)
 endef
