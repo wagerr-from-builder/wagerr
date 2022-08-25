21d9a2caf38ee68922b40b94ea63a0082147340b
wagerrck <104495875+wagerrck@users.noreply.github.com>
2022-08-25 13:25:10 +0100

    Scale overview icon 9

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -602,7 +602,7 @@ void BitcoinGUI::createToolBars()
 
         overviewButton = new QToolButton(this);
         overviewButton->setText(tr("&Overview"));
-        overviewButton->setIcon(QIcon(":/images/overview"));
+        overviewButton->setIcon(QIcon(":/images/overview.xpm"));
         overviewButton->setStatusTip(tr("Show general overview of wallet"));
         tabGroup->addButton(overviewButton);
 
