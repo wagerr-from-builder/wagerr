bytzck/BYTZ

Update Zerocoin

src/txdb.cpp
@@ -13,6 +13,7 @@
 #include <util.h>
 #include <ui_interface.h>
 #include <init.h>
+#include <zbytz/accumulators.h>
 
 #include <stdint.h>
 
@@ -420,6 +421,7 @@ bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, 
     pcursor->Seek(std::make_pair(DB_BLOCK_INDEX, uint256()));
 
     // Load mapBlockIndex
+    uint256 nPreviousCheckpoint;
     while (pcursor->Valid()) {
         boost::this_thread::interruption_point();
         std::pair<char, uint256> key;
@@ -443,10 +445,27 @@ bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, 
 
                 //Proof Of Stake
                 pindexNew->nFlags         = diskindex.nFlags;
+                if (pindexNew->nHeight < Params().GetConsensus().nBlockStakeModifierV2) {
+                    pindexNew->nStakeModifier = diskindex.nStakeModifier;
+                } else {
+                    pindexNew->nStakeModifierV2 = diskindex.nStakeModifierV2;
+                }
+                //Zerocoin
+                pindexNew->nAccumulatorCheckpoint = diskindex.nAccumulatorCheckpoint;
+                pindexNew->mapZerocoinSupply = diskindex.mapZerocoinSupply;
+                pindexNew->vMintDenominationsInBlock = diskindex.vMintDenominationsInBlock;
 
                 if (pindexNew->nHeight < consensusParams.nPosStartHeight && pindexNew->IsProofOfWork() && !CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits, consensusParams))
                     return error("%s: CheckProofOfWork failed: %s", __func__, pindexNew->ToString());
 
+                //populate accumulator checksum map in memory
+                if(pindexNew->nAccumulatorCheckpoint != uint256() && pindexNew->nAccumulatorCheckpoint != nPreviousCheckpoint) {
+                    //Don't load any checkpoints that exist before v2 zbytz. The accumulator is invalid for v1 and not used.
+                    if (pindexNew->nHeight >= Params().GetConsensus().nBlockZerocoinV2)
+                        LoadAccumulatorValuesFromDB(pindexNew->nAccumulatorCheckpoint);
+
+                    nPreviousCheckpoint = pindexNew->nAccumulatorCheckpoint;
+                }
                 pcursor->Next();
             } else {
                 return error("%s: failed to read value", __func__);

wagerr-from-builder/wagerr

src/txdb.cpp
@@ -14,6 +14,7 @@
 #include <ui_interface.h>
 #include <util/translation.h>
 #include <util/vector.h>
+#include <zwgr/accumulators.h>
 
 #include <stdint.h>
 
@@ -384,6 +385,7 @@ bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, 
     pcursor->Seek(std::make_pair(DB_BLOCK_INDEX, uint256()));
 
     // Load m_block_index
+    uint256 nPreviousCheckpoint;
     while (pcursor->Valid()) {
         boost::this_thread::interruption_point();
         if (ShutdownRequested()) return false;
@@ -408,10 +410,27 @@ bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, 
 
                 //Proof Of Stake
                 pindexNew->nFlags         = diskindex.nFlags;
+                if (pindexNew->nHeight < Params().GetConsensus().nBlockStakeModifierV2) {
+                    pindexNew->nStakeModifier = diskindex.nStakeModifier;
+                } else {
+                    pindexNew->nStakeModifierV2 = diskindex.nStakeModifierV2;
+                }
+                //Zerocoin
+                pindexNew->nAccumulatorCheckpoint = diskindex.nAccumulatorCheckpoint;
+                pindexNew->mapZerocoinSupply = diskindex.mapZerocoinSupply;
+                pindexNew->vMintDenominationsInBlock = diskindex.vMintDenominationsInBlock;
 
                 if (pindexNew->nHeight < consensusParams.nPosStartHeight && pindexNew->IsProofOfWork() && !CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits, consensusParams))
                     return error("%s: CheckProofOfWork failed: %s", __func__, pindexNew->ToString());
 
+                //populate accumulator checksum map in memory
+                if(pindexNew->nAccumulatorCheckpoint != uint256() && pindexNew->nAccumulatorCheckpoint != nPreviousCheckpoint) {
+                    //Don't load any checkpoints that exist before v2 zwgr. The accumulator is invalid for v1 and not used.
+                    if (pindexNew->nHeight >= Params().GetConsensus().nBlockZerocoinV2)
+                        LoadAccumulatorValuesFromDB(pindexNew->nAccumulatorCheckpoint);
+
+                    nPreviousCheckpoint = pindexNew->nAccumulatorCheckpoint;
+                }
                 pcursor->Next();
             } else {
                 return error("%s: failed to read value", __func__);