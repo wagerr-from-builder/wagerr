bytzck/BYTZ

Tests for shared library

test/functional/test_framework/__init__.py
@@ -1,0 +1,6 @@
+# Copyright (c) 2018 The Bitcoin Unlimited developers
+# Copyright (c) 2021 The Bytz Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+from test_framework.bytzlib import init, bin2hex, signTxInput, randombytes, pubkey, spendscript, addrbin, txid, SIGHASH_ALL, SIGHASH_NONE, SIGHASH_SINGLE, SIGHASH_ANYONECANPAY

wagerr-from-builder/wagerr

test/functional/test_framework/__init__.py
@@ -1,0 +1,6 @@
+# Copyright (c) 2018 The Bitcoin Unlimited developers
+# Copyright (c) 2021 The Bytz Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+from test_framework.wagerrlib import init, bin2hex, signTxInput, randombytes, pubkey, spendscript, addrbin, txid, SIGHASH_ALL, SIGHASH_NONE, SIGHASH_SINGLE, SIGHASH_ANYONECANPAY