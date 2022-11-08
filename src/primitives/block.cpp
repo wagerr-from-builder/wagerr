791fa1b252654142ee8e9f66afd6c80cbf2eee12
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2022-11-08 11:27:52 +0000

    Wagerr chain parameters
    -add Quark hash
    -change magic numbers
    -change hashes to wagerr hashes
    -change version

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -14,7 +14,9 @@ uint256 CBlockHeader::GetHash() const
     std::vector<unsigned char> vch(80);
     CVectorWriter ss(SER_GETHASH, PROTOCOL_VERSION, vch, 0);
     ss << *this;
-    return HashX11((const char *)vch.data(), (const char *)vch.data() + vch.size());
+    if (this->nVersion >= 4)
+        return Hash((const char *)vch.data(), (const char *)vch.data() + vch.size());
+    return HashQuark((const char *)vch.data(), (const char *)vch.data() + vch.size());
 }
 
 std::string CBlock::ToString() const
