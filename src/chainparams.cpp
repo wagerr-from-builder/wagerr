a72db317595d54d939680df4a1bb2ab369d04886
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2022-11-15 15:47:48 +0000

    5

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -183,10 +183,15 @@ public:
         consensus.nPowTargetSpacing = 1 * 60; // Wagerr: 1 minute
         consensus.fPowAllowMinDifficultyBlocks = false;
         consensus.fPowNoRetargeting = false;
+        // Wagerr specific parameters
+        // Proof of Stake parameters
         consensus.nPosStartHeight = 201;
-        consensus.nPivxProtocolV2 = std::numeric_limits<int>::max();;
-        consensus.nRuleChangeActivationThreshold = 1916; // 95% of 2016
-        consensus.nMinerConfirmationWindow = 2016; // nPowTargetTimespan / nPowTargetSpacing
+        consensus.nPivxProtocolV2StartHeight = std::numeric_limits<int>::max();
+        consensus.posLimit = uint256S("000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); // ~uint256(0) >> 24
+        consensus.nPosTargetSpacing = 1 * 60; // 1 minute
+        consensus.nStakeMinDepth = 600;
+        consensus.nBlockStakeModifierV1A = 1000;
+        consensus.nBlockStakeModifierV2 = std::numeric_limits<int>::max();
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 1199145601; // January 1, 2008
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 1230767999; // December 31, 2008
