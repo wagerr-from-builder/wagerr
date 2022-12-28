bcbd388797766d241b7110196467d8a01b24fce3
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2022-12-28 15:06:54 +0000

    10

--- depends/packages/cmake.mk
+++ depends/packages/cmake.mk
@@ -2,8 +2,7 @@ package=cmake
 $(package)_version=3.14.7
 $(package)_download_path=https://github.com/Kitware/CMake/releases/download/v3.14.7/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=9221993e0af3e6d10124d840ff24f5b2f3b884416fca04d3312cb0
-388dec1385
+$(package)_sha256_hash=9221993e0af3e6d10124d840ff24f5b2f3b884416fca04d3312cb0388dec1385
 $(package)_patches=cmake-debian-gcc-8.patch
 
 define $(package)_preprocess_cmds
