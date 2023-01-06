96565473a9ee90db5831dfa4af78e1be8fac87f2
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-01-06 12:37:01 +0000

    6

--- src/test/block_reward_reallocation_tests.cpp
+++ src/test/block_reward_reallocation_tests.cpp
@@ -31,7 +31,7 @@
 
 using SimpleUTXOMap = std::map<COutPoint, std::pair<int, CAmount>>;
 
-const auto deployment_id = Consensus::DEPLOYMENT_REALLOC;
+const auto deployment_id = Consensus::DEPLOYMENT_DIP0024;
 const int window{500}, th_start{400}, th_end{300};
 
 struct TestChainBRRBeforeActivationSetup : public TestChainSetup
