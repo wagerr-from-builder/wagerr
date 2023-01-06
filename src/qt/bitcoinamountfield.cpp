86341a3f7f498f3d17889eef29f87b8ad424539e
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-01-06 13:59:34 +0000

    19

--- src/qt/bitcoinamountfield.cpp
+++ src/qt/bitcoinamountfield.cpp
@@ -2,9 +2,9 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <qt/wagerramountfield.h>
+#include <qt/bitcoinamountfield.h>
 
-#include <qt/wagerrunits.h>
+#include <qt/bitcoinunits.h>
 #include <qt/guiutil.h>
 
 #include <QApplication>
